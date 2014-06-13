35197b4b704d71561430d5d8323cb80e09af882f
Lee David Painter <lee@ludup.com>
2014-06-13 14:09:09 +0100

    Now have usable getPropertiesWithValue method in repository hooked up to RealmService so it can be used to search principals.

--- hypersocket-core/src/main/java/com/hypersocket/properties/ResourceTemplateRepository.java
+++ hypersocket-core/src/main/java/com/hypersocket/properties/ResourceTemplateRepository.java
@@ -4,7 +4,7 @@ import java.util.Collection;
 
 import com.hypersocket.resource.AbstractResource;
 
-public interface ResourceTemplateRepository {
+public interface ResourceTemplateRepository extends PropertyRepository {
 
 	void loadPropertyTemplates(String xmlResourcePath);
 	
