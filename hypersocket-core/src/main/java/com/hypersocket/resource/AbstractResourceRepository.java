fa4e81704d59ba5c8fff1d609e6290459a61cf03
Lee Painter <lee@ludup.com>
2014-06-23 15:09:59 +0100

    Added AbstractResourceService / Repository implementations

---
+++ hypersocket-core/src/main/java/com/hypersocket/resource/AbstractResourceRepository.java
@@ -0,0 +1,32 @@
+package com.hypersocket.resource;
+
+import java.util.List;
+
+import com.hypersocket.realm.Realm;
+import com.hypersocket.repository.AbstractRepository;
+import com.hypersocket.repository.CriteriaConfiguration;
+import com.hypersocket.tables.ColumnSort;
+
+public interface AbstractResourceRepository<T extends Resource> extends AbstractRepository<Long> {
+
+	T getResourceByName(String name);
+
+	T getResourceByName(String name, boolean deleted);
+
+	T getResourceById(Long id);
+
+	void deleteResource(T resource) throws ResourceChangeException;
+
+	void saveResource(T resource);
+
+	List<T> getResources(Realm realm);
+
+	List<T> getResources();
+
+	List<T> search(Realm realm, String searchPattern, int start, int length,
+			ColumnSort[] sorting, CriteriaConfiguration... configs);
+
+	long getResourceCount(Realm realm, String searchPattern,
+			CriteriaConfiguration... configs);
+
+}
---
+++ hypersocket-core/src/main/java/com/hypersocket/resource/AbstractResourceService.java
@@ -0,0 +1,32 @@
+package com.hypersocket.resource;
+
+import java.util.List;
+
+import com.hypersocket.auth.AuthenticatedService;
+import com.hypersocket.permissions.AccessDeniedException;
+import com.hypersocket.realm.Realm;
+import com.hypersocket.tables.ColumnSort;
+
+public interface AbstractResourceService<T extends Resource> extends AuthenticatedService {
+
+	void createResource(T resource) throws ResourceCreationException,
+			AccessDeniedException;
+
+	void deleteResource(T resource) throws ResourceChangeException,
+			AccessDeniedException;
+
+	List<T> getResources(Realm realm) throws AccessDeniedException;
+
+	List<T> searchResources(Realm realm, String search, int start, int length,
+			ColumnSort[] sorting) throws AccessDeniedException;
+
+	long getResourceCount(Realm realm, String search)
+			throws AccessDeniedException;
+
+	List<T> getResources();
+
+	T getResourceByName(String name) throws ResourceNotFoundException;
+
+	T getResourceById(Long id) throws ResourceNotFoundException;
+
+}
