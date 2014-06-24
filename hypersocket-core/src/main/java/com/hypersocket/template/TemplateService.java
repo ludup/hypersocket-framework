c4b654d59d2f0e4328d3c14f90811bc1ea88fa39
Lee Painter <lee@ludup.com>
2014-06-24 23:07:21 +0100

    Editing of templates first draft completed.

--- hypersocket-core/src/main/java/com/hypersocket/template/TemplateService.java
+++ hypersocket-core/src/main/java/com/hypersocket/template/TemplateService.java
@@ -1,19 +1,27 @@
 package com.hypersocket.template;
 
+import java.util.Map;
 import java.util.Set;
 
 import com.hypersocket.permissions.AccessDeniedException;
+import com.hypersocket.realm.MediaNotFoundException;
+import com.hypersocket.realm.Principal;
 import com.hypersocket.resource.AbstractResourceService;
 import com.hypersocket.resource.ResourceChangeException;
 import com.hypersocket.resource.ResourceCreationException;
+import com.hypersocket.resource.ResourceNotFoundException;
 
 public interface TemplateService extends AbstractResourceService<Template> {
 
 	Set<String> getRegisteredTypes();
 
-	Template createTemplate(String name, String template, String type) throws ResourceCreationException, AccessDeniedException;
+	Template createTemplate(String name, String subject, String template, String type) throws ResourceCreationException, AccessDeniedException;
 
-	void updateTemplate(Template template, String name, String template2,
+	void updateTemplate(Template template, String name, String subject, String template2,
 			String type) throws ResourceChangeException, AccessDeniedException;
+
+	void emailTemplate(String templateName, Principal principal,
+			Map<String, String> replacements) throws ResourceNotFoundException,
+			MediaNotFoundException;
 
 }
