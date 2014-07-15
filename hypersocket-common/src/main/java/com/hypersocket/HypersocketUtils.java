f059b7fd665bc33a014728ffbbc373c4162e8696
Lee Painter <lee@ludup.com>
2014-07-15 16:10:38 +0100

    Ability to set entity values in get/setters as if they were properties.

---
+++ hypersocket-common/src/main/java/com/hypersocket/HypersocketUtils.java
@@ -0,0 +1,25 @@
+package com.hypersocket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class HypersocketUtils {
+
+	
+	static Logger log = LoggerFactory.getLogger(HypersocketUtils.class);
+	
+	static ThreadLocal<Long> times = new ThreadLocal<Long>();
+	
+	
+	public static void resetInterval() {
+		times.set(System.currentTimeMillis());
+	}
+	
+	public static void logInterval(String msg) {
+		
+		log.info("REMOVE ME: " + msg + ": " + (System.currentTimeMillis() - times.get()));
+		resetInterval();
+	}
+	
+	
+}
