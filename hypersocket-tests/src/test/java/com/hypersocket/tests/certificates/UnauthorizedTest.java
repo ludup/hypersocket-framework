8f4704e7d135d1c044b5bb718c6297ee129a5ef6
ArunaAbesekara <aruna@nervepoint.com>
2014-08-28 15:27:32 +0100

    Certificate testing

---
+++ hypersocket-tests/src/test/java/com/hypersocket/tests/certificates/UnauthorizedTest.java
@@ -0,0 +1,22 @@
+package com.hypersocket.tests.certificates;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.message.BasicNameValuePair;
+import org.junit.Test;
+
+import com.hypersocket.json.JsonResponse;
+import com.hypersocket.tests.AbstractServerTest;
+import com.hypersocket.tests.MultipartObject;
+import com.hypersocket.tests.PropertyObject;
+
+public class UnauthorizedTest  extends AbstractCertificateTest{
+	
+	
+	
+}
