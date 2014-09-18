9d142c7d86e609b65c8412167732e6f20d93347d
Lee David Painter <lee@ludup.com>
2014-09-18 08:25:48 +0100

    New certificate resource to replace static certificate.

---
+++ hypersocket-core/src/main/java/com/hypersocket/certificates/CertificateResourceRepository.java
@@ -0,0 +1,8 @@
+package com.hypersocket.certificates;
+
+import com.hypersocket.resource.AbstractResourceRepository;
+
+
+public interface CertificateResourceRepository extends
+		AbstractResourceRepository<CertificateResource> {
+}
