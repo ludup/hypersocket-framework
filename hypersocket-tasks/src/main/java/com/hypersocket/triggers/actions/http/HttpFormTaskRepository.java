50156a9930452a2c23c00b2f10dae708961c547e
Lee David Painter <lee@ludup.com>
2015-01-27 17:31:00 +0000

    Removed HttpTasks from core framework and placed into plugin.

--- hypersocket-tasks/src/main/java/com/hypersocket/triggers/actions/http/HttpFormTaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hypersocket.triggers.actions.http;
-
-import com.hypersocket.properties.ResourceTemplateRepository;
-
-public interface HttpFormTaskRepository extends ResourceTemplateRepository {
-
-}
