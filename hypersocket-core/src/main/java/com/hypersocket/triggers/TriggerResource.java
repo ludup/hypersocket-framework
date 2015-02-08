b1912d64463320cfe329b829872135c538485a51
Lee Painter <lee@ludup.com>
2015-02-08 14:40:01 +0000

    Renamed all trigger tables.

--- hypersocket-core/src/main/java/com/hypersocket/triggers/TriggerResource.java
+++ hypersocket-core/src/main/java/com/hypersocket/triggers/TriggerResource.java
@@ -14,7 +14,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.hypersocket.resource.RealmResource;
 
 @Entity
-@Table(name="trigger_resources")
+@Table(name="trigger_resource")
 public class TriggerResource extends RealmResource {
 
 	@Column(name="result")
