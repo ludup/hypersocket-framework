65709edacbf935a9c343fdf2635cc1c32536e3ad
aruna_abesekara <aruna@nervepoint.com>
2015-03-12 14:51:36 +0000

    Permission test cases

--- hypersocket-tests/src/test/java/com/hypersocket/tests/json/permissions/WithDelegatedPermissionTest.java
+++ hypersocket-tests/src/test/java/com/hypersocket/tests/json/permissions/WithDelegatedPermissionTest.java
@@ -5,8 +5,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.hypersocket.auth.AuthenticationPermission;
-import com.hypersocket.json.JsonResourceStatus;
-import com.hypersocket.json.JsonRoleResourceStatus;
 import com.hypersocket.realm.UserPermission;
 import com.hypersocket.tests.AbstractServerTest;
 
@@ -14,17 +12,9 @@ public class WithDelegatedPermissionTest extends AbstractServerTest {
 
 	@BeforeClass
 	public static void init() throws Exception {
-		logon("System", "admin", "Password123?");
-		JsonResourceStatus jsonCreateUser = createUser("System", "user1",
-				"password1", false);
-		Long[] permissions = {
-				getPermissionId(AuthenticationPermission.LOGON.getResourceKey()),
-				getPermissionId(UserPermission.CREATE.getResourceKey()) };
-		JsonRoleResourceStatus jsonCreateRole = createRole("newrole",
-				permissions);
-		addUserToRole(jsonCreateRole.getResource(), jsonCreateUser);
-		logoff();
-		logon("System", "user1", "password1");
+		logOnNewUser(new String[] {
+				AuthenticationPermission.LOGON.getResourceKey(),
+				UserPermission.CREATE.getResourceKey() });
 	}
 
 	@AfterClass
