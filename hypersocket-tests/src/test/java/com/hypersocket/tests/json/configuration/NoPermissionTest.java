a7bf0124e3c711dd786b2875ea4e2d6fc40fe7df
aruna_abesekara <aruna@nervepoint.com>
2015-03-13 15:26:53 +0000

    Configuration API Test cases

--- hypersocket-tests/src/test/java/com/hypersocket/tests/json/configuration/NoPermissionTest.java
+++ hypersocket-tests/src/test/java/com/hypersocket/tests/json/configuration/NoPermissionTest.java
@@ -1,9 +1,25 @@
 package com.hypersocket.tests.json.configuration;
 
 import org.apache.http.client.ClientProtocolException;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class NoPermissionTest extends AbstractConfigurationTest {
+import com.hypersocket.auth.AuthenticationPermission;
+import com.hypersocket.tests.AbstractServerTest;
+
+public class NoPermissionTest extends AbstractServerTest {
+
+	@BeforeClass
+	public static void init() throws Exception {
+		logOnNewUser(new String[] { AuthenticationPermission.LOGON
+				.getResourceKey() });
+	}
+
+	@AfterClass
+	public static void clean() throws Exception {
+		logoff();
+	}
 
 	@Test(expected = ClientProtocolException.class)
 	public void testgetConfiguationWithoutLogon() throws Exception {
