9113f19a286227ab3af16edb4ad9af8d5482ae2a
aruna_abesekara <aruna@nervepoint.com>
2015-03-18 11:51:50 +0000

    Reimplement certificate testings

--- hypersocket-tests/src/test/java/com/hypersocket/tests/json/certificates/WithAdminPermissionTest.java
+++ hypersocket-tests/src/test/java/com/hypersocket/tests/json/certificates/WithAdminPermissionTest.java
@@ -1,10 +1,5 @@
 package com.hypersocket.tests.json.certificates;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
-import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.security.Permission;
@@ -13,17 +8,28 @@ import java.util.Map;
 
 import org.apache.http.message.BasicNameValuePair;
 import org.junit.AfterClass;
+
+import static org.junit.Assert.*;
+
 import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import com.hypersocket.certificates.CertificateType;
+import com.hypersocket.json.JsonCertificate;
+import com.hypersocket.json.JsonResourceStatus;
+import com.hypersocket.json.JsonResourceStatusTemplate;
 import com.hypersocket.json.JsonResponse;
+import com.hypersocket.properties.json.PropertyItem;
+import com.hypersocket.resource.ResourceUpdate;
 import com.hypersocket.tests.AbstractServerTest;
-import com.hypersocket.tests.json.utils.MultipartObject;
-import com.hypersocket.tests.json.utils.PropertyObject;
 
-public class WithAdminPermissionTest extends AbstractServerTest {
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class WithAdminPermissionTest extends AbstractCertificateTest {
+	static long resourceID = 0;
 
 	@BeforeClass
 	public static void LogOn() throws Exception {
@@ -38,100 +44,114 @@ public class WithAdminPermissionTest extends AbstractServerTest {
 	}
 
 	@Test
-	public void testGetConfiguration() throws Exception {
-		String json = doGet("/hypersocket/api/certificates");
-		debugJSON(json);
-		assertNotNull(json);
-		JsonResponse resp = getMapper().readValue(json, JsonResponse.class);
-		assertTrue(resp.isSuccess());
+	public void test01_createCertificate() throws Exception {
+		ResourceUpdate update = new ResourceUpdate();
+		update.setName("TestCertificate");
+		PropertyItem[] properties = new PropertyItem[7];
+		properties[0] = new PropertyItem();
+		properties[0].setId("commonName");
+		properties[0].setValue("common name");
+		properties[1] = new PropertyItem();
+		properties[1].setId("organizationalUnit");
+		properties[1].setValue("unit1");
+		properties[2] = new PropertyItem();
+		properties[2].setId("organization");
+		properties[2].setValue("organization");
+		properties[3] = new PropertyItem();
+		properties[3].setId("location");
+		properties[3].setValue("unknown");
+		properties[4] = new PropertyItem();
+		properties[4].setId("state");
+		properties[4].setValue("State");
+		properties[5] = new PropertyItem();
+		properties[5].setId("country");
+		properties[5].setValue("AF");
+		properties[6] = new PropertyItem();
+		properties[6].setId("certType");
+		properties[6].setValue(CertificateType.RSA_1024.name());
+
+		update.setProperties(properties);
+		String result = doPostJson("/hypersocket/api/certificates/certificate",
+				update);
+		debugJSON(result);
+		JsonResourceStatus json = getMapper().readValue(result,
+				JsonResourceStatus.class);
+		assertTrue(json.isSuccess());
+		assertEquals("TestCertificate", json.getResource().getName());
+		resourceID = json.getResource().getId();
 	}
 
 	@Test
-	public void testKeyGeneration() throws Exception {
-		String json = doPost("/hypersocket/api/certificates/generateCSR",
-				new BasicNameValuePair("cn", "common name"),// common name
-				new BasicNameValuePair("ou", "unit1"), // Organizational unit
-				new BasicNameValuePair("o", "organization"), // Organization
-				new BasicNameValuePair("l", "location1"), // location
-				new BasicNameValuePair("s", "State"), // State
-				new BasicNameValuePair("c", "AF")); // country
-		JsonResponse resp = getMapper().readValue(json, JsonResponse.class);
-		debugJSON(json);
-		assertTrue(resp.isSuccess());
-		assertEquals(0,
-				resp.getMessage()
-						.indexOf("-----BEGIN CERTIFICATE REQUEST-----"));
-
+	public void test02_getCertificate() throws Exception {
+		assertNotEquals(0, resourceID);
+		String result = doGet("/hypersocket/api/certificates/certificate/"
+				+ resourceID);
+		debugJSON(result);
+		JsonCertificate json = getMapper().readValue(result,
+				JsonCertificate.class);
+		assertEquals("TestCertificate", json.getName());
 	}
 
 	@Test
-	public void testUploadKey() throws Exception {
-
-		File file = new File(System.getProperty("user.dir") + File.separator
-				+ "key_files" + File.separator + "server.crt");
-		MultipartObject filePro = new MultipartObject("file", file);
-
-		File bundleFile = new File(System.getProperty("user.dir")
-				+ File.separator + "key_files" + File.separator
-				+ "ca_bundle.crt");
-		MultipartObject fileBundle = new MultipartObject("bundle", bundleFile);
-
-		File keyFile = new File(System.getProperty("user.dir") + File.separator
-				+ "key_files" + File.separator + "server.key");
-		MultipartObject Key = new MultipartObject("key", keyFile);
-
-		PropertyObject passphrase = new PropertyObject("passphrase", "");
-
-		PropertyObject[] properties = new PropertyObject[] { passphrase };
-		String json = doPostMultiparts("/hypersocket/api/certificates/key",
-				properties, filePro, fileBundle, Key);
-		debugJSON(json);
-		JsonResponse resp = getMapper().readValue(json, JsonResponse.class);
-		assertTrue(resp.isSuccess());
-		assertEquals(
-				"Your key has been accepted. The server will need to be restarted.",
-				resp.getMessage());
-		json = doGet("/hypersocket/api/certificates");
-		debugJSON(json);
+	public void test03_updateCertificate() throws Exception {
+		assertNotEquals(0, resourceID);
+		ResourceUpdate update = new ResourceUpdate();
+		update.setName("TestCertificate");
+		update.setId(resourceID);
+		PropertyItem[] properties = new PropertyItem[7];
+		properties[0] = new PropertyItem();
+		properties[0].setId("commonName");
+		properties[0].setValue("common name");
+		properties[1] = new PropertyItem();
+		properties[1].setId("organizationalUnit");
+		properties[1].setValue("unit1");
+		properties[2] = new PropertyItem();
+		properties[2].setId("organization");
+		properties[2].setValue("organization");
+		properties[3] = new PropertyItem();
+		properties[3].setId("location");
+		properties[3].setValue("unknown");
+		properties[4] = new PropertyItem();
+		properties[4].setId("state");
+		properties[4].setValue("Nottingham");
+		properties[5] = new PropertyItem();
+		properties[5].setId("country");
+		properties[5].setValue("UK");
+		properties[6] = new PropertyItem();
+		properties[6].setId("certType");
+		properties[6].setValue(CertificateType.RSA_1024.name());
+		update.setProperties(properties);
+		String result = doPostJson("/hypersocket/api/certificates/certificate",
+				update);
+		debugJSON(result);
+		JsonResponse json = getMapper().readValue(result, JsonResponse.class);
+		assertTrue(json.isSuccess());
+		assertEquals("Updated Certificate TestCertificate", json.getMessage());
 	}
 
 	@Test
-	public void testUploadPfx() throws Exception {
-
-		File keyFile = new File(System.getProperty("user.dir") + File.separator
-				+ "key_files" + File.separator + "nervepoint-www-wildcard.pfx");
-		MultipartObject filePro = new MultipartObject("key", keyFile);
-
-		PropertyObject passphrase = new PropertyObject("passphrase",
-				"bluemars73");
-		PropertyObject[] properties = new PropertyObject[] { passphrase };
-
-		String json = doPostMultiparts("/hypersocket/api/certificates/pfx",
-				properties, filePro);
-		debugJSON(json);
-		JsonResponse resp = getMapper().readValue(json, JsonResponse.class);
-		assertTrue(resp.isSuccess());
-		assertEquals(
-				"Your key has been accepted. The server will need to be restarted.",
-				resp.getMessage());
-
+	public void test04_testDownloadCSR() throws Exception {
+		assertNotEquals(0, resourceID);
+		String result = doGet("/hypersocket/api/certificates/downloadCSR/"
+				+ resourceID);
+		assertTrue(result.startsWith("-----BEGIN CERTIFICATE REQUEST-----"));
 	}
 
 	@Test
-	public void testUploadCertificate() throws Exception {
+	public void test05_testDownloadCertificate() throws Exception {
+		assertNotEquals(0, resourceID);
+		String result = doGet("/hypersocket/api/certificates/downloadCertificate/"
+				+ resourceID);
+		assertTrue(result.startsWith("-----BEGIN CERTIFICATE-----"));
+	}
 
-		File keyFile = new File(System.getProperty("user.dir") + File.separator
-				+ "key_files" + File.separator + "server.crt");
-		MultipartObject filePro = new MultipartObject("file", keyFile);
-
-		File BundleFile = new File(System.getProperty("user.dir")
-				+ File.separator + "key_files" + File.separator
-				+ "ca_bundle.crt");
-		MultipartObject KeyBundle = new MultipartObject("bundle", BundleFile);
-		String json = doPostMultiparts("/hypersocket/api/certificates/cert",
-				null, filePro, KeyBundle);
-		debugJSON(json);
-
+	@Test
+	public void test06_testExportPEM() throws Exception {
+		assertNotEquals(0, resourceID);
+		JsonResponse json = getMapper().readValue(doPost("/hypersocket/api/certificates/exportPfx/"+resourceID,
+				new BasicNameValuePair("passphrase", "passsword")), JsonResponse.class);
+		assertTrue(json.isSuccess());
+		doGet("/hypersocket/api/certificates/downloadPfx/"+ resourceID);
 	}
 
 	private static void removeCryptographyRestrictions() {
