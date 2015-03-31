9061486d4c34e1d07592743a500dfb1049eab075
Lee Painter <lee@ludup.com>
2015-03-31 13:33:48 +0100

    Added HttpUtils helper method to GET content from remote URL.

--- hypersocket-core/src/main/java/com/hypersocket/http/HttpUtils.java
+++ hypersocket-core/src/main/java/com/hypersocket/http/HttpUtils.java
@@ -1,6 +1,7 @@
 package com.hypersocket.http;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -10,6 +11,7 @@ import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.conn.ssl.SSLContextBuilder;
@@ -23,59 +25,101 @@ import org.apache.http.util.EntityUtils;
 public class HttpUtils {
 
 	static BasicCookieStore cookieStore;
-	
+
 	public static CloseableHttpClient createHttpClient(boolean allowSelfSigned)
 			throws IOException {
 		CloseableHttpClient httpclient = null;
 
-		if(allowSelfSigned) {
+		if (allowSelfSigned) {
 			try {
 				SSLContextBuilder builder = new SSLContextBuilder();
 				builder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
 				SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(
 						builder.build(),
 						SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
-				httpclient = HttpClients.custom()
-						.setSSLSocketFactory(sslsf)
+				httpclient = HttpClients.custom().setSSLSocketFactory(sslsf)
 						.setDefaultCookieStore(cookieStore).build();
 			} catch (Exception e) {
 				throw new IOException(e);
 			}
-		
+
 		} else {
 			httpclient = HttpClients.custom()
 					.setDefaultCookieStore(cookieStore).build();
 		}
-		
+
 		return httpclient;
 	}
-	
-	public static String doHttpPost(String url, Map<String,String> parameters, boolean allowSelfSigned) throws IOException {
-		
+
+	public static String doHttpPost(String url, Map<String, String> parameters,
+			boolean allowSelfSigned) throws IOException {
+
 		CloseableHttpClient client = createHttpClient(allowSelfSigned);
-		
+
 		try {
 			HttpPost request = new HttpPost(url);
-			
+
 			List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
-			
-			for(String name : parameters.keySet()) {
-				nameValuePairs.add(new BasicNameValuePair(name, parameters.get(name)));
+
+			for (String name : parameters.keySet()) {
+				nameValuePairs.add(new BasicNameValuePair(name, parameters
+						.get(name)));
 			}
 			request.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 			HttpResponse response = client.execute(request);
-			
-			if(response.getStatusLine().getStatusCode()!=HttpStatus.SC_OK) {
-				throw new IOException("Received " + response.getStatusLine().toString());
+
+			if (response.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
+				throw new IOException("Received "
+						+ response.getStatusLine().toString());
 			}
 			HttpEntity entity = response.getEntity();
 			return EntityUtils.toString(entity);
-		
+
 		} finally {
 			try {
 				client.close();
 			} catch (IOException e) {
 			}
 		}
+	}
+
+	public static InputStream doHttpGet(String uri, boolean allowSelfSigned)
+			throws IOException {
+
+		CloseableHttpClient client = createHttpClient(allowSelfSigned);
+
+		HttpGet request = new HttpGet(uri);
+		return new ContentInputStream(client, client.execute(request)
+				.getEntity().getContent());
+
+	}
+
+	static class ContentInputStream extends InputStream {
+
+		CloseableHttpClient client;
+		InputStream wrapped;
+
+		ContentInputStream(CloseableHttpClient client, InputStream wrapped) {
+			this.client = client;
+			this.wrapped = wrapped;
+		}
+
+		@Override
+		public int read() throws IOException {
+			int b = wrapped.read();
+			if (b == -1) {
+				client.close();
+			}
+			return b;
+		}
+
+		public int read(byte[] buf, int off, int len) throws IOException {
+			int b = wrapped.read(buf, off, len);
+			if (b == -1) {
+				client.close();
+			}
+			return b;
+		}
+
 	}
 }
