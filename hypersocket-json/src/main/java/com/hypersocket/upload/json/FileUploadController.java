406ef49b3df1515586efadd1524ba3ef1291582a
ludup <lee@javassh.com>
2015-04-03 14:19:14 +0100

    Fix logging import.

--- hypersocket-json/src/main/java/com/hypersocket/upload/json/FileUploadController.java
+++ hypersocket-json/src/main/java/com/hypersocket/upload/json/FileUploadController.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
