a64ee4574c6f0b390c3cc766e14487d1f4e43f33
Javier Izaguirre <javier.izaguirre@nervepoint.com>
2015-06-04 11:20:37 +0100

    sandy_590 Overview Dashboard with plugable display widgets

--- hypersocket-core/src/main/java/com/hypersocket/dashboard/OverviewWidgetService.java
+++ hypersocket-core/src/main/java/com/hypersocket/dashboard/OverviewWidgetService.java
@@ -1,5 +1,12 @@
 package com.hypersocket.dashboard;
 
+import java.util.List;
+
+import com.hypersocket.permissions.AccessDeniedException;
+
 public interface OverviewWidgetService {
 
+	public void registerWidget(OverviewWidget widget);
+
+	public List<OverviewWidget> getWidgets() throws AccessDeniedException;
 }
