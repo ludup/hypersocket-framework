7ae014970c9367c04ece79a474e8de447554e00c
Javier Izaguirre <javier.izaguirre@nervepoint.com>
2015-06-04 11:20:37 +0100

    sandy_590 Overview Dashboard with plugable display widgets

--- hypersocket-core/src/main/java/com/hypersocket/dashboard/OverviewWidget.java
+++ hypersocket-core/src/main/java/com/hypersocket/dashboard/OverviewWidget.java
@@ -1,11 +1,59 @@
 package com.hypersocket.dashboard;
 
-public class OverviewWidget {
+public class OverviewWidget implements Comparable<OverviewWidget>{
 
 	int weight;
 	String resourceKey;
 	String contentPath;
 	boolean large;
-	
-	
+
+	public OverviewWidget() {
+
+	}
+
+	public OverviewWidget(int weight, String resourceKey, String contentPath,
+			boolean large) {
+
+		this.weight = weight;
+		this.resourceKey = resourceKey;
+		this.contentPath = contentPath;
+		this.large = large;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public String getResourceKey() {
+		return resourceKey;
+	}
+
+	public void setResourceKey(String resourceKey) {
+		this.resourceKey = resourceKey;
+	}
+
+	public String getContentPath() {
+		return contentPath;
+	}
+
+	public void setContentPath(String contentPath) {
+		this.contentPath = contentPath;
+	}
+
+	public boolean isLarge() {
+		return large;
+	}
+
+	public void setLarge(boolean large) {
+		this.large = large;
+	}
+
+	@Override
+	public int compareTo(OverviewWidget overviewWidget) {
+		return weight<overviewWidget.getWeight()?-1:weight>overviewWidget.getWeight()?1:0;
+	}
 }
