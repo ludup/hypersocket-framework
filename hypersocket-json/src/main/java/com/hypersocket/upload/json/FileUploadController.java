238e8d07ee3c61b2db24742473c3c8b0f935fd2f
Lee David Painter <lee@ludup.com>
2015-07-01 14:35:08 +0100

    Exporting of file uploads

--- hypersocket-json/src/main/java/com/hypersocket/upload/json/FileUploadController.java
+++ hypersocket-json/src/main/java/com/hypersocket/upload/json/FileUploadController.java
@@ -1,10 +1,12 @@
 package com.hypersocket.upload.json;
 
 import java.io.IOException;
+import java.util.Collection;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -22,16 +25,19 @@ import com.hypersocket.auth.json.AuthenticationRequired;
 import com.hypersocket.auth.json.ResourceController;
 import com.hypersocket.auth.json.UnauthorizedException;
 import com.hypersocket.i18n.I18N;
+import com.hypersocket.i18n.I18NServiceImpl;
 import com.hypersocket.json.ResourceStatus;
 import com.hypersocket.permissions.AccessDeniedException;
 import com.hypersocket.realm.Realm;
 import com.hypersocket.resource.ResourceCreationException;
 import com.hypersocket.resource.ResourceException;
+import com.hypersocket.resource.ResourceExportException;
 import com.hypersocket.resource.ResourceNotFoundException;
 import com.hypersocket.session.json.SessionTimeoutException;
 import com.hypersocket.upload.FileUpload;
 import com.hypersocket.upload.FileUploadService;
 import com.hypersocket.upload.FileUploadServiceImpl;
+import com.hypersocket.utils.HypersocketUtils;
 
 @Controller
 public class FileUploadController extends ResourceController {
@@ -39,7 +45,7 @@ public class FileUploadController extends ResourceController {
 	static Logger log = LoggerFactory.getLogger(FileUploadController.class);
 	
 	@Autowired
-	FileUploadService service;
+	FileUploadService resourceService;
 
 	@AuthenticationRequired
 	@RequestMapping(value = "fileUpload/metainfo/{uuid}", method = RequestMethod.GET, produces = { "application/json" })
@@ -53,7 +59,7 @@ public class FileUploadController extends ResourceController {
 		setupAuthenticatedContext(sessionUtils.getSession(request),
 				sessionUtils.getLocale(request));
 		try {
-			return new ResourceStatus<FileUpload>(service.getFileByUuid(uuid));
+			return new ResourceStatus<FileUpload>(resourceService.getFileByUuid(uuid));
 
 		} catch(ResourceException ex) { 
 			return new ResourceStatus<FileUpload>(false, ex.getMessage());
@@ -79,7 +85,7 @@ public class FileUploadController extends ResourceController {
 			FileUpload fileUpload;
 
 			Realm realm = sessionUtils.getCurrentRealm(request);
-			fileUpload = service.createFile(file, realm);
+			fileUpload = resourceService.createFile(file, realm);
 
 			return new ResourceStatus<FileUpload>(fileUpload, I18N.getResource(
 					sessionUtils.getLocale(request),
@@ -113,7 +119,7 @@ public class FileUploadController extends ResourceController {
 		setupAuthenticatedContext(sessionUtils.getSession(request),
 				sessionUtils.getLocale(request));
 		try {
-			FileUpload fileUpload = service.getFileByUuid(uuid);
+			FileUpload fileUpload = resourceService.getFileByUuid(uuid);
 			if (fileUpload == null) {
 				return new ResourceStatus<FileUpload>(false, I18N.getResource(
 						sessionUtils.getLocale(request),
@@ -122,7 +128,7 @@ public class FileUploadController extends ResourceController {
 			}
 
 			String preDeletedName = fileUpload.getFileName();
-			service.deleteFile(fileUpload);
+			resourceService.deleteFile(fileUpload);
 			return new ResourceStatus<FileUpload>(true, I18N.getResource(
 					sessionUtils.getLocale(request),
 					FileUploadServiceImpl.RESOURCE_BUNDLE,
@@ -155,8 +161,104 @@ public class FileUploadController extends ResourceController {
 				sessionUtils.getLocale(request));
 		try {
 
-			service.downloadURIFile(uuid, request, response, true);
+			resourceService.downloadURIFile(uuid, request, response, true);
 
+		} finally {
+			clearAuthenticatedContext();
+		}
+	}
+	
+	@AuthenticationRequired
+	@RequestMapping(value = "fileUpload/export/{id}", method = RequestMethod.GET, produces = { "text/plain" })
+	@ResponseStatus(value = HttpStatus.OK)
+	@ResponseBody
+	public String export(HttpServletRequest request,
+			HttpServletResponse response, @PathVariable("id") long id)
+			throws AccessDeniedException, UnauthorizedException,
+			SessionTimeoutException, ResourceNotFoundException,
+			ResourceExportException {
+
+		setupAuthenticatedContext(sessionUtils.getSession(request),
+				sessionUtils.getLocale(request));
+		try {
+			Thread.sleep(1000);
+		} catch (Exception e) {
+		}
+		try {
+			response.setHeader("Content-Disposition", "attachment; filename=\""
+					+ resourceService.getResourceCategory() + "-"
+					+ resourceService.getResourceById(id).getName() + ".json\"");
+			return resourceService.exportResoure(id);
+		} finally {
+			clearAuthenticatedContext();
+		}
+	}
+	
+	@AuthenticationRequired
+	@RequestMapping(value = "fileUpload/export", method = RequestMethod.GET, produces = { "text/plain" })
+	@ResponseStatus(value = HttpStatus.OK)
+	@ResponseBody
+	public String exportAll(HttpServletRequest request,
+			HttpServletResponse response)
+			throws AccessDeniedException, UnauthorizedException,
+			SessionTimeoutException, ResourceNotFoundException,
+			ResourceExportException {
+
+		setupAuthenticatedContext(sessionUtils.getSession(request),
+				sessionUtils.getLocale(request));
+		try {
+			Thread.sleep(1000);
+		} catch (Exception e) {
+		}
+		try {
+			response.setHeader("Content-Disposition", "attachment; filename=\""
+					+ resourceService.getResourceCategory() + "-all" + ".json\"");
+			return resourceService.exportResources(resourceService.allResources());
+		} finally {
+			clearAuthenticatedContext();
+		}
+	}
+	
+	@AuthenticationRequired
+	@RequestMapping(value = "fileUpload/import", method = { RequestMethod.POST }, produces = { "application/json" })
+	@ResponseBody
+	@ResponseStatus(value = HttpStatus.OK)
+	public ResourceStatus<FileUpload> importLaunchers(
+			HttpServletRequest request, HttpServletResponse response,
+			@RequestParam(value = "file") MultipartFile jsonFile,
+			@RequestParam(required=false) boolean dropExisting)
+			throws AccessDeniedException, UnauthorizedException,
+			SessionTimeoutException {
+		setupAuthenticatedContext(sessionUtils.getSession(request),
+				sessionUtils.getLocale(request));
+		try {
+			Thread.sleep(2000);
+		} catch (Exception e) {
+		}
+		try {
+			String json = IOUtils.toString(jsonFile.getInputStream());
+			if (!HypersocketUtils.isValidJSON(json)) {
+				throw new ResourceException(I18NServiceImpl.USER_INTERFACE_BUNDLE,
+						"error.incorrectJSON");
+			}
+			Collection<FileUpload> collects = resourceService
+					.importResources(json, getCurrentRealm(), dropExisting);
+			return new ResourceStatus<FileUpload>(
+					true,
+					I18N.getResource(
+							sessionUtils.getLocale(request),
+							I18NServiceImpl.USER_INTERFACE_BUNDLE,
+							"resource.import.success", collects.size()));
+		} catch (ResourceException e) {
+			return new ResourceStatus<FileUpload>(false, e.getMessage());
+		} catch (Exception e) {
+			return new ResourceStatus<FileUpload>(
+					false,
+					I18N.getResource(
+							sessionUtils.getLocale(request),
+							I18NServiceImpl.USER_INTERFACE_BUNDLE,
+							"resource.import.failure",
+							e.getMessage()));
 		} finally {
 			clearAuthenticatedContext();
 		}
