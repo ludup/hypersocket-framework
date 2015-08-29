7856d90054f39ccf03f8ccf8105d9aab790a1c10
Lee David Painter <lee@ludup.com>
2015-08-29 17:00:05 +0100

    EntityResourcePropertyStore is no longer a component bean. It is now created in each AbstractRepository implementation so that property resource key conflicts are kept to a minimum. i.e. we can have a resourceKey of "description" in ProductResource and in ActiveDirectoryUser without the properties of one affecting the other.

--- hypersocket-core/src/main/java/com/hypersocket/automation/AutomationResourceService.java
+++ hypersocket-core/src/main/java/com/hypersocket/automation/AutomationResourceService.java
@@ -4,7 +4,6 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
-import com.hypersocket.events.EventDefinition;
 import com.hypersocket.permissions.AccessDeniedException;
 import com.hypersocket.properties.PropertyCategory;
 import com.hypersocket.realm.Realm;
