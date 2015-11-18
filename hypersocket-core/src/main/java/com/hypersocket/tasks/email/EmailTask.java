6cff8c52beff4f467578a025aba0059ca31174e6
Lee David Painter <lee@ludup.com>
2015-11-18 17:31:14 +0000

    Cleared out all warnings.

--- hypersocket-core/src/main/java/com/hypersocket/tasks/email/EmailTask.java
+++ hypersocket-core/src/main/java/com/hypersocket/tasks/email/EmailTask.java
@@ -1,7 +1,6 @@
 package com.hypersocket.tasks.email;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
