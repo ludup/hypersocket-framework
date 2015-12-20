f5433efaf425d5732cdfc3455f8b7c1de6ea046b
ludup <lee@ludup.com>
2015-12-20 16:27:51 +0000

    I think this will fix the browser launchable list. Problem is that Hibernate is returning maxResults * number of implementations of BrowserLaunchable when queries are created using the BrowserLaunchable interface. We have to revert to getting and sub listing the whole list of results in this case.

--- hypersocket-core/src/main/java/com/hypersocket/browser/BrowserLaunchableRepositoryImpl.java
+++ hypersocket-core/src/main/java/com/hypersocket/browser/BrowserLaunchableRepositoryImpl.java
@@ -1,7 +1,6 @@
 package com.hypersocket.browser;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -9,8 +8,6 @@ import java.util.Set;
 import org.apache.commons.lang3.StringUtils;
 import org.hibernate.Criteria;
 import org.hibernate.criterion.CriteriaSpecification;
-import org.hibernate.criterion.Order;
-import org.hibernate.criterion.ProjectionList;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
@@ -20,92 +17,27 @@ import com.hypersocket.realm.Principal;
 import com.hypersocket.repository.AbstractRepositoryImpl;
 import com.hypersocket.repository.CriteriaConfiguration;
 import com.hypersocket.tables.ColumnSort;
-import com.hypersocket.tables.Sort;
 
 @Repository
 public class BrowserLaunchableRepositoryImpl extends
 		AbstractRepositoryImpl<Long> implements BrowserLaunchableRepository {
 
 
-	@SuppressWarnings("unchecked")
 	@Override
 	@Transactional(readOnly=true)
 	public List<BrowserLaunchable> searchAssignedResources(List<Principal> principals,
 			final String searchPattern, final int start, final int length,
 			final ColumnSort[] sorting, CriteriaConfiguration... configs) {
 
-		Criteria criteria = createCriteria(BrowserLaunchable.class);
-		
-		if (StringUtils.isNotBlank(searchPattern)) {
-			criteria.add(Restrictions.ilike("name", searchPattern));
-		}
 
-		for (CriteriaConfiguration c : configs) {
-			c.configure(criteria);
-		}
-		
-		criteria.add(Restrictions.or(
-				Restrictions.eq("displayInBrowserResourcesTable", true), 
-				Restrictions.isNull("displayInBrowserResourcesTable")));
-
-		criteria.setProjection(Projections.distinct(Projections.id()));
-		criteria.setResultTransformer(CriteriaSpecification.PROJECTION);
-		
-		criteria.add(Restrictions.eq("realm", principals.get(0).getRealm()));
-		criteria = criteria.createCriteria("roles");
-		criteria.add(Restrictions.eq("allUsers", true));
-		
-		Set<Long> ids = new HashSet<Long>(criteria.list());
-
-		
-		criteria = createCriteria(BrowserLaunchable.class);
-		
-		if (StringUtils.isNotBlank(searchPattern)) {
-			criteria.add(Restrictions.ilike("name", searchPattern));
-		}
-
-		for (CriteriaConfiguration c : configs) {
-			c.configure(criteria);
-		}
-		
-		criteria.add(Restrictions.or(
-				Restrictions.eq("displayInBrowserResourcesTable", true), 
-				Restrictions.isNull("displayInBrowserResourcesTable")));
-
-		criteria.setProjection(Projections.distinct(Projections.id()));
-		criteria.setResultTransformer(CriteriaSpecification.PROJECTION);
-
-		criteria.add(Restrictions.eq("realm", principals.get(0).getRealm()));
-
-		criteria = criteria.createCriteria("roles");
-		criteria.add(Restrictions.eq("allUsers", false));
-		criteria = criteria.createCriteria("principals");
-		
-		List<Long> principalIds = new ArrayList<Long>();
-		for(Principal p : principals) {
-			principalIds.add(p.getId());
-		}
-		criteria.add(Restrictions.in("id", principalIds));
-		
-		ids.addAll(criteria.list());
-			
-		if(ids.size() > 0) {
-			
-			criteria.add(Restrictions.in("id", ids));
-
-			criteria.setFirstResult(start);
-			criteria.setMaxResults(length);
-			
-			for (ColumnSort sort : sorting) {
-				criteria.addOrder(sort.getSort() == Sort.ASC ? Order.asc(sort
-						.getColumn().getColumnName()) : Order.desc(sort.getColumn()
-						.getColumnName()));
-			}
-			
-			return ((List<BrowserLaunchable>) criteria.list());
-		}
-		
-		return new ArrayList<BrowserLaunchable>();
+		/**
+		 * There appears to be no way around this. DISTINCT_ROOT_ENTITY is
+		 * returning max results * number of implementations of BrowserLaunchable
+		 * so instead get them all and chop the collection to the required
+		 * length and starting position.
+		 */
+		List<BrowserLaunchable> resources = getPersonalResources(principals);
+		return resources.subList(start, start+length);
 	};
 
 
@@ -139,7 +71,6 @@ public class BrowserLaunchableRepositoryImpl extends
 		
 		Set<Long> ids = new HashSet<Long>(criteria.list());
 
-		
 		criteria = createCriteria(BrowserLaunchable.class);
 		
 		if (StringUtils.isNotBlank(searchPattern)) {
@@ -179,24 +110,39 @@ public class BrowserLaunchableRepositoryImpl extends
 	@Transactional(readOnly=true)
 	public List<BrowserLaunchable> getPersonalResources(List<Principal> principals) {
 		
+		/**
+		 * Get any resources assigned to Everyone role.
+		 */
 		Criteria criteria = createCriteria(BrowserLaunchable.class);
-		criteria.setResultTransformer(CriteriaSpecification.DISTINCT_ROOT_ENTITY);
 
+		criteria.setProjection(Projections.distinct(Projections.id()));
+		criteria.setResultTransformer(CriteriaSpecification.PROJECTION);
+		
 		criteria.add(Restrictions.eq("realm", principals.get(0).getRealm()));
+		
+		criteria.add(Restrictions.or(
+				Restrictions.eq("displayInBrowserResourcesTable", true), 
+				Restrictions.isNull("displayInBrowserResourcesTable")));
 		
 		criteria = criteria.createCriteria("roles");
 		criteria.add(Restrictions.eq("allUsers", true));
 		
-		List<BrowserLaunchable> everyone = new ArrayList<BrowserLaunchable>(criteria.list());
+		HashSet<Long> allResources = new HashSet<Long>(criteria.list());
 
+		/**
+		 * Now look for additional resources attached to any Role that has
+		 * any of the principals listed.
+		 */
 		criteria = createCriteria(BrowserLaunchable.class);
 		
-		ProjectionList projList = Projections.projectionList();
-		projList.add(Projections.property("id"));
-		projList.add(Projections.property("name"));
+		criteria.setProjection(Projections.distinct(Projections.id()));
+		criteria.setResultTransformer(CriteriaSpecification.PROJECTION);
 		
-		criteria.setProjection(Projections.distinct(projList));
 		criteria.add(Restrictions.eq("realm", principals.get(0).getRealm()));
+		
+		criteria.add(Restrictions.or(
+				Restrictions.eq("displayInBrowserResourcesTable", true), 
+				Restrictions.isNull("displayInBrowserResourcesTable")));
 		
 		criteria = criteria.createCriteria("roles");
 		criteria.add(Restrictions.eq("allUsers", false));
@@ -208,23 +154,23 @@ public class BrowserLaunchableRepositoryImpl extends
 		}
 		criteria.add(Restrictions.in("id", ids));
 		
-		List<Object[]> results = (List<Object[]>)criteria.list();
+		allResources.addAll(criteria.list());
 		
-		if(results.size() > 0) {
-			Long[] entityIds = new Long[results.size()];
-			int idx = 0;
-			for(Object[] obj : results) {
-				entityIds[idx++] = (Long) obj[0];
-			}
-			
+		/**
+		 * Now return all the distinct resources based on the unique set of 
+		 * resource ids.
+		 */
+		if(allResources.size() > 0) {
+						
 			criteria = createCriteria(BrowserLaunchable.class);
 			criteria.setResultTransformer(CriteriaSpecification.DISTINCT_ROOT_ENTITY);
 			
-			criteria.add(Restrictions.in("id", entityIds));
+			criteria.add(Restrictions.in("id", allResources));
 	
-			everyone.addAll((List<BrowserLaunchable>) criteria.list());
+			return criteria.list();
 		}
-		return everyone;
+		
+		return new ArrayList<BrowserLaunchable>();
 	}
 
 }
