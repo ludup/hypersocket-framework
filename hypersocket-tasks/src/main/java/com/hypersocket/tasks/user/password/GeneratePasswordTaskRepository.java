4519f39442e0d5d49d1497a5b0f1a35f3600ee89
ludup <lee@ludup.com>
2015-12-30 14:46:23 +0000

    Added Generate Password Task.

---
+++ hypersocket-tasks/src/main/java/com/hypersocket/tasks/user/password/GeneratePasswordTaskRepository.java
@@ -0,0 +1,7 @@
+package com.hypersocket.tasks.user.password;
+
+import com.hypersocket.properties.ResourceTemplateRepository;
+
+public interface GeneratePasswordTaskRepository extends ResourceTemplateRepository {
+
+}
