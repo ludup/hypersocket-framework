3b80135ba944e83b1ed7119e4b6d06062263895e
Lee David Painter <lee@ludup.com>
2016-02-11 15:42:05 +0000

    Ok so roles list was needed. Different approach will be required.

--- hypersocket-core/src/main/java/com/hypersocket/resource/AssignableResource.java
+++ hypersocket-core/src/main/java/com/hypersocket/resource/AssignableResource.java
@@ -22,7 +22,6 @@ import javax.persistence.Table;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.hypersocket.permissions.Role;
 
 @Entity
@@ -39,7 +38,6 @@ public class AssignableResource extends RealmResource {
 	public AssignableResource() {
 	}
 	
-	@JsonIgnore
 	public Set<Role> getRoles() {
 		return roles;
 	}
