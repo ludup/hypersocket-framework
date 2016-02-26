c32ed6c97cc387b891f59c82b6069d28026d214e
Javier Izaguirre <javier.izaguirre@nervepoint.com>
2016-02-26 15:25:44 +0000

    figaro_798 Working version of the table preferences

--- hypersocket-core/src/main/java/com/hypersocket/interfaceState/UserInterfaceStateRepositoryImpl.java
+++ hypersocket-core/src/main/java/com/hypersocket/interfaceState/UserInterfaceStateRepositoryImpl.java
@@ -5,6 +5,7 @@ import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.hypersocket.realm.Realm;
 import com.hypersocket.repository.CriteriaConfiguration;
 import com.hypersocket.resource.AbstractResourceRepositoryImpl;
 
@@ -14,9 +15,29 @@ public class UserInterfaceStateRepositoryImpl extends
 		UserInterfaceStateRepository {
 
 	@Override
-	@Transactional(readOnly=true)
+	@Transactional(readOnly = true)
 	public UserInterfaceState getStateByResourceId(final Long resourceId) {
 		return get("id", resourceId, UserInterfaceState.class);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public UserInterfaceState getState(final String name,
+			final Long principalId, final String resourceCategory,
+			final Realm realm) {
+		UserInterfaceState userInterface = get(getResourceClass(),
+				new CriteriaConfiguration() {
+					@Override
+					public void configure(Criteria criteria) {
+						criteria.add(Restrictions.eq("name", name))
+								.add(Restrictions
+										.eq("principalId", principalId))
+								.add(Restrictions.eq("resourceCategory",
+										resourceCategory))
+								.add(Restrictions.eq("realm", realm));
+					}
+				});
+		return userInterface;
 	}
 
 	@Override
