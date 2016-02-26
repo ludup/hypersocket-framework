d5ec6d5f51fe9673c4fa5b8e56ad6e4dc4eae562
Javier Izaguirre <javier.izaguirre@nervepoint.com>
2016-02-26 09:26:42 +0000

    figaro_798 Save table state implemented

--- hypersocket-core/src/main/java/com/hypersocket/interfaceState/UserInterfaceState.java
+++ hypersocket-core/src/main/java/com/hypersocket/interfaceState/UserInterfaceState.java
@@ -10,53 +10,33 @@ import com.hypersocket.resource.RealmResource;
 @Table(name = "user_interface_state")
 public class UserInterfaceState extends RealmResource {
 
-	@Column(name = "top")
-	Long top;
-
-	@Column(name = "leftpx")
-	Long leftpx;
-
-	@Column(name = "resourceId")
-	Long resourceId;
+//	@Column(name = "resourceId")
+//	Long resourceId;
 
 	@Column(name = "principalId")
 	Long principalId;
 
+	@Column(name = "preferences")
+	String preferences;
+
 	public UserInterfaceState() {
+
 	}
 
-	public UserInterfaceState(Long top, Long leftpx, Long resourceId,
-			Long principalId) {
-
-		this.top = top;
-		this.leftpx = leftpx;
-		this.resourceId = resourceId;
+	public UserInterfaceState(/*Long resourceId, */Long principalId,
+			String preferences) {
+//		this.resourceId = resourceId;
 		this.principalId = principalId;
+		this.preferences = preferences;
 	}
 
-	public Long getTop() {
-		return top;
-	}
-
-	public void setTop(Long top) {
-		this.top = top;
-	}
-
-	public Long getLeftpx() {
-		return leftpx;
-	}
-
-	public void setLeftpx(Long leftpx) {
-		this.leftpx = leftpx;
-	}
-
-	public Long getResourceId() {
-		return resourceId;
-	}
-
-	public void setResourceId(Long resourceId) {
-		this.resourceId = resourceId;
-	}
+//	public Long getResourceId() {
+//		return resourceId;
+//	}
+//
+//	public void setResourceId(Long resourceId) {
+//		this.resourceId = resourceId;
+//	}
 
 	public Long getPrincipalId() {
 		return principalId;
@@ -66,4 +46,11 @@ public class UserInterfaceState extends RealmResource {
 		this.principalId = principalId;
 	}
 
+	public String getPreferences() {
+		return preferences;
+	}
+
+	public void setPreferences(String preferences) {
+		this.preferences = preferences;
+	}
 }
--- hypersocket-json/src/main/java/com/hypersocket/interfaceState/json/UserInterfaceStateController.java
+++ hypersocket-json/src/main/java/com/hypersocket/interfaceState/json/UserInterfaceStateController.java
@@ -16,11 +16,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import com.hypersocket.auth.json.AuthenticationRequired;
 import com.hypersocket.auth.json.ResourceController;
 import com.hypersocket.auth.json.UnauthorizedException;
-import com.hypersocket.i18n.I18N;
 import com.hypersocket.interfaceState.UserInterfaceState;
 import com.hypersocket.interfaceState.UserInterfaceStateService;
-import com.hypersocket.interfaceState.UserInterfaceStateServiceImpl;
-import com.hypersocket.json.ResourceList;
 import com.hypersocket.json.ResourceStatus;
 import com.hypersocket.permissions.AccessDeniedException;
 import com.hypersocket.session.json.SessionTimeoutException;
@@ -31,67 +28,109 @@ public class UserInterfaceStateController extends ResourceController {
 	@Autowired
 	UserInterfaceStateService service;
 
+	/*
+	 * @AuthenticationRequired
+	 * 
+	 * @RequestMapping(value = "interfaceState/state/{specific}/{resources}",
+	 * method = RequestMethod.GET, produces = { "application/json" })
+	 * 
+	 * @ResponseBody
+	 * 
+	 * @ResponseStatus(value = HttpStatus.OK) public
+	 * ResourceList<UserInterfaceState> getStates( HttpServletRequest request,
+	 * 
+	 * @PathVariable("specific") boolean specific,
+	 * 
+	 * @PathVariable("resources") Long[] resources) throws
+	 * AccessDeniedException, UnauthorizedException, SessionTimeoutException {
+	 * setupAuthenticatedContext(sessionUtils.getSession(request),
+	 * sessionUtils.getLocale(request)); try { return new
+	 * ResourceList<UserInterfaceState>(service.getStates( resources)); }
+	 * finally { clearAuthenticatedContext(); } }
+	 */
+	/*
+	 * @AuthenticationRequired
+	 * 
+	 * @RequestMapping(value = "interfaceState/state", method =
+	 * RequestMethod.POST, produces = { "application/json" })
+	 * 
+	 * @ResponseBody
+	 * 
+	 * @ResponseStatus(value = HttpStatus.OK) public
+	 * ResourceStatus<UserInterfaceState> saveState( HttpServletRequest request,
+	 * HttpServletResponse response,
+	 * 
+	 * @RequestBody UserInterfaceStateUpdate userInterfaceStateUpdate) throws
+	 * AccessDeniedException, UnauthorizedException, SessionTimeoutException {
+	 * 
+	 * setupAuthenticatedContext(sessionUtils.getSession(request),
+	 * sessionUtils.getLocale(request)); try {
+	 * 
+	 * UserInterfaceState newState = service
+	 * .getStateByResourceId(userInterfaceStateUpdate .getResourceId());
+	 * 
+	 * if (newState != null) { newState = service.updateState(newState,
+	 * userInterfaceStateUpdate.getName(),
+	 * userInterfaceStateUpdate.getProperties()); } else { newState =
+	 * service.createState( userInterfaceStateUpdate.getResourceId(),
+	 * userInterfaceStateUpdate.getName(),
+	 * userInterfaceStateUpdate.getProperties()); } return new
+	 * ResourceStatus<UserInterfaceState>(newState,
+	 * I18N.getResource(sessionUtils.getLocale(request),
+	 * UserInterfaceStateServiceImpl.RESOURCE_BUNDLE, newState.getId() != null ?
+	 * "attribute.updated.info" : "attribute.created.info", newState
+	 * .getName())); // } catch (ResourceException e) { // return new
+	 * ResourceStatus<UserInterfaceState>(false, // e.getMessage());
+	 * 
+	 * } finally { clearAuthenticatedContext(); } }
+	 */
 	@AuthenticationRequired
-	@RequestMapping(value = "interfaceState/state/{specific}/{resources}", method = RequestMethod.GET, produces = { "application/json" })
+	@RequestMapping(value = "interfaceState/tableState/{name}", method = RequestMethod.GET, produces = { "application/json" })
 	@ResponseBody
 	@ResponseStatus(value = HttpStatus.OK)
-	public ResourceList<UserInterfaceState> getStates(
-			HttpServletRequest request,
-			@PathVariable("specific") boolean specific,
-			@PathVariable("resources") Long[] resources)
+	public ResourceStatus<UserInterfaceState> getTableStates(
+			HttpServletRequest request, @PathVariable("name") String name)
 			throws AccessDeniedException, UnauthorizedException,
 			SessionTimeoutException {
 		setupAuthenticatedContext(sessionUtils.getSession(request),
 				sessionUtils.getLocale(request));
 		try {
-			return new ResourceList<UserInterfaceState>(
-					service.getStates(resources, specific));
+			UserInterfaceState userInterfaceState = service.getState(name);
+			return new ResourceStatus<UserInterfaceState>(userInterfaceState,
+					userInterfaceState.getName());
 		} finally {
 			clearAuthenticatedContext();
 		}
 	}
 
 	@AuthenticationRequired
-	@RequestMapping(value = "interfaceState/state", method = RequestMethod.POST, produces = { "application/json" })
+	@RequestMapping(value = "interfaceState/tableState", method = RequestMethod.POST, produces = { "application/json" })
 	@ResponseBody
 	@ResponseStatus(value = HttpStatus.OK)
-	public ResourceStatus<UserInterfaceState> saveState(
+	public ResourceStatus<UserInterfaceState> saveTableState(
 			HttpServletRequest request, HttpServletResponse response,
-			@RequestBody UserInterfaceStateUpdate userInterfaceStateUpdate)
+			@RequestBody UserInterfaceStateUpdate userInterfaceState)
 			throws AccessDeniedException, UnauthorizedException,
 			SessionTimeoutException {
 
 		setupAuthenticatedContext(sessionUtils.getSession(request),
 				sessionUtils.getLocale(request));
 		try {
-
-			UserInterfaceState newState = service
-					.getStateByResourceId(userInterfaceStateUpdate
-							.getResourceId());
-
+			UserInterfaceState newState = service.getState(userInterfaceState
+					.getName() + "_" + getCurrentPrincipal().getId());
 			if (newState != null) {
 				newState = service.updateState(newState,
-						userInterfaceStateUpdate.getTop(),
-						userInterfaceStateUpdate.getLeftpx(),
-						userInterfaceStateUpdate.getName(),
-						userInterfaceStateUpdate.isSpecific());
+						userInterfaceState.getPreferences());
 			} else {
-				newState = service.createState(
-						userInterfaceStateUpdate.getResourceId(),
-						userInterfaceStateUpdate.getTop(),
-						userInterfaceStateUpdate.getLeftpx(),
-						userInterfaceStateUpdate.getName(),
-						userInterfaceStateUpdate.isSpecific());
+				newState = service.createState(getCurrentPrincipal().getId(),
+						userInterfaceState.getPreferences(),
+						userInterfaceState.getName());
 			}
+
 			return new ResourceStatus<UserInterfaceState>(newState,
-					I18N.getResource(sessionUtils.getLocale(request),
-							UserInterfaceStateServiceImpl.RESOURCE_BUNDLE,
-							newState.getId() != null ? "attribute.updated.info"
-									: "attribute.created.info", newState
-									.getName()));
-			// } catch (ResourceException e) {
-			// return new ResourceStatus<UserInterfaceState>(false,
-			// e.getMessage());
+					newState.getName());
+		} catch (Exception e) {
+			return new ResourceStatus<UserInterfaceState>(false, e.getMessage());
 
 		} finally {
 			clearAuthenticatedContext();
