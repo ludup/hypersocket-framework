43c7ed95148c14a1660a3e0dd13055da4a3286e9
ludup <lee@ludup.com>
2016-05-29 20:23:51 +0100

    Separation of resource reconcile and realm reconcile events. Some fixes from open tickets, including role deletion constraints.

--- hypersocket-core/src/main/java/com/hypersocket/automation/AutomationResourceRepositoryImpl.java
+++ hypersocket-core/src/main/java/com/hypersocket/automation/AutomationResourceRepositoryImpl.java
@@ -1,16 +1,26 @@
 package com.hypersocket.automation;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.hibernate.Criteria;
 import org.hibernate.FetchMode;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.hypersocket.resource.AbstractResourceRepositoryImpl;
+import com.hypersocket.resource.TransactionOperation;
+import com.hypersocket.triggers.TriggerResource;
+import com.hypersocket.triggers.TriggerResourceRepository;
 
 @Repository
 public class AutomationResourceRepositoryImpl extends
 		AbstractResourceRepositoryImpl<AutomationResource> implements
 		AutomationResourceRepository {
 
+	@Autowired
+	TriggerResourceRepository triggerRepository;
+	
 	@Override
 	protected Class<AutomationResource> getResourceClass() {
 		return AutomationResource.class;
@@ -19,6 +29,43 @@ public class AutomationResourceRepositoryImpl extends
 	@Override
 	protected void processDefaultCriteria(Criteria criteria) {
 		criteria.setFetchMode("triggers", FetchMode.SELECT);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void deleteResource(AutomationResource resource, TransactionOperation<AutomationResource>... ops) {
+		
+		
+		Collection<TriggerResource> triggers = new ArrayList<TriggerResource>(resource.getChildTriggers());
+		for(TriggerResource trigger : triggers) {
+			recurseDetach(trigger);
+		}
+		
+		resource.getChildTriggers().clear();
+		save(resource);
+		flush();
+		super.deleteResource(resource, ops);
+		
+		for(TriggerResource trigger : triggers) {
+			recurseDelete(trigger);
+		}
+		flush();
+		
+	}
+
+	private void recurseDelete(TriggerResource trigger) {
+		for(TriggerResource child : trigger.getChildTriggers()) {
+			recurseDelete(child);
+		}
+		delete(trigger);
+	}
+
+	private void recurseDetach(TriggerResource trigger) {
+		for(TriggerResource child : trigger.getChildTriggers()) {
+			recurseDetach(child);
+		}
+		trigger.setParentTrigger(null);
+		save(trigger);
 	}
 
 }
