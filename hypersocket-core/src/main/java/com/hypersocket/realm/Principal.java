f26e1bd97671192964af7d69d86e647db0263796
ludup <lee@ludup.com>
2016-10-04 16:42:33 +0100

    Make getType @Transient

--- hypersocket-core/src/main/java/com/hypersocket/realm/Principal.java
+++ hypersocket-core/src/main/java/com/hypersocket/realm/Principal.java
@@ -54,6 +54,7 @@ public abstract class Principal extends RealmResource {
 		return super.getRealm();
 	}
 
+	@Transient
 	public abstract PrincipalType getType();
 
 	public abstract String getIcon();
