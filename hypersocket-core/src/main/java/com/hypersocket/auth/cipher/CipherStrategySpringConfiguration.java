5623dcb9c50a79a20f1db5e45796d0209273ab31
gaurav@hypersocket.com <gaurav@hypersocket.com>
2016-10-20 17:15:48 +0530

    Renamed java file and explicitly gave beans name

--- hypersocket-core/src/main/java/com/hypersocket/auth/cipher/CipherStrategySpringConfiguration.java
+++ hypersocket-core/src/main/java/com/hypersocket/auth/cipher/CipherStrategySpringConfiguration.java
@@ -5,30 +5,30 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class PBKDF2CipherStrategyConfiguration {
+public class CipherStrategySpringConfiguration {
 
     public static final int KEY_LENGTH = 160;
 
     @Qualifier(CipherStrategy.BCRYPT_QUALIFIER)
-    @Bean
+    @Bean(name = CipherStrategy.BCRYPT_QUALIFIER)
     public CipherStrategy bcrypt(){
         return new BCryptCipherStrategy();
     }
 
     @Qualifier(CipherStrategy.PBKDF2_256_QUALIFIER)
-    @Bean
+    @Bean(name = CipherStrategy.PBKDF2_256_QUALIFIER)
     public CipherStrategy pbkdf2Sha256(){
        return new PBKDF2CipherStrategy(KEY_LENGTH, PBKDF2CipherStrategy.Algorithm.SHA_256);
     }
 
     @Qualifier(CipherStrategy.PBKDF2_384_QUALIFIER)
-    @Bean
+    @Bean(name = CipherStrategy.PBKDF2_384_QUALIFIER)
     public CipherStrategy pbkdf2Sha384(){
         return new PBKDF2CipherStrategy(KEY_LENGTH, PBKDF2CipherStrategy.Algorithm.SHA_384);
     }
 
     @Qualifier(CipherStrategy.PBKDF2_512_QUALIFIER)
-    @Bean
+    @Bean(name = CipherStrategy.PBKDF2_512_QUALIFIER)
     public CipherStrategy pbkdf2Sha512(){
         return new PBKDF2CipherStrategy(KEY_LENGTH, PBKDF2CipherStrategy.Algorithm.SHA_512);
     }
