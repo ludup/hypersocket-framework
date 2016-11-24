94fd1cd9c4a40233f77b582626b6e2ca06c1b133
ludup <lee@ludup.com>
2016-11-24 17:36:21 +0000

    {}

--- hypersocket-core/src/main/java/com/hypersocket/properties/ResourceTemplateRepositoryImpl.java
+++ hypersocket-core/src/main/java/com/hypersocket/properties/ResourceTemplateRepositoryImpl.java
@@ -658,8 +658,9 @@ public abstract class ResourceTemplateRepositoryImpl extends PropertyRepositoryI
 				if (propertyTemplates.containsKey(resourceKey)) {
 					setValue(resource, resourceKey, properties.get(resourceKey));
 				}
-				else
+				else {
 					log.warn(String.format("Request to set property %s which does not exist in template %s", resourceKey, resource.toString()));
+				}
 			}
 		}
 	}
