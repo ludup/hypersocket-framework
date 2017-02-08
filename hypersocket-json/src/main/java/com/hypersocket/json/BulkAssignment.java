a136ecded6944266bb539ca6fd4e3daffe85ea61
gaurav@hypersocket.com <gaurav@hypersocket.com>
2017-02-08 22:42:14 +0530

    Added concept of Displayable enums where apart from name and ordinal we can havea display value which can be used in drop downs mainly for display purpose. Also added http interface to fetch list of Displayables in EnumController.
    Added BulkAssignment helper POJO and ENum to capture Resource and Role ids and mode.
    
    UserInterface i18n file added messages for bulk assignment.

---
+++ hypersocket-json/src/main/java/com/hypersocket/json/BulkAssignment.java
@@ -0,0 +1,37 @@
+package com.hypersocket.json;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class BulkAssignment {
+
+    private List<Long> resourceIds;
+    private List<Long> roleIds;
+    private BulkAssignmentMode mode;
+
+    public List<Long> getResourceIds() {
+        return resourceIds;
+    }
+
+    public void setResourceIds(List<Long> resourceIds) {
+        this.resourceIds = resourceIds;
+    }
+
+    public List<Long> getRoleIds() {
+        return roleIds;
+    }
+
+    public void setRoleIds(List<Long> roleIds) {
+        this.roleIds = roleIds;
+    }
+
+    public BulkAssignmentMode getMode() {
+        return mode;
+    }
+
+    public void setMode(BulkAssignmentMode mode) {
+        this.mode = mode;
+    }
+}
---
+++ hypersocket-json/src/main/java/com/hypersocket/json/BulkAssignmentMode.java
@@ -0,0 +1,51 @@
+package com.hypersocket.json;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.hypersocket.properties.enumeration.Displayable;
+
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum BulkAssignmentMode implements Displayable<BulkAssignmentMode> {
+    Merge("Merge"), OverWrite("Over Write");
+
+    private String display;
+    private int id;
+    private static BulkAssignmentMode[] cache = BulkAssignmentMode.values();
+
+    BulkAssignmentMode(String display) {
+        this.display = display;
+        this.id = this.ordinal();
+    }
+
+    @Override
+    public String getDisplay() {
+        return display;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String getName() {
+        return name();
+    }
+
+    @Override
+    public BulkAssignmentMode fromOrdinal(int ordinal){
+        return cache[ordinal];
+    }
+
+    @JsonCreator
+    public static BulkAssignmentMode forValue(String value) {
+        int id = Integer.parseInt(value);
+        return cache[id];
+    }
+
+    @Override
+    public String toString() {
+        return Integer.toString(this.ordinal());
+    }
+
+}
