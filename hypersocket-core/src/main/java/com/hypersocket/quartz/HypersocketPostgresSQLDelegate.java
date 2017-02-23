7118dd7c00d7e971967c775844cafb7980230d68
gaurav@hypersocket.com <gaurav@hypersocket.com>
2017-02-23 20:29:09 +0530

    When extension is removed, old jobs in database should be marked Noop, handling case for postgres

---
+++ hypersocket-core/src/main/java/com/hypersocket/quartz/HypersocketPostgresSQLDelegate.java
@@ -0,0 +1,60 @@
+package com.hypersocket.quartz;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class HypersocketPostgresSQLDelegate extends HypersocketStdJDBCDelegate {
+
+    static Logger log = LoggerFactory.getLogger(HypersocketPostgresSQLDelegate.class);
+
+    /**
+     * Below code is copied as it is from org.quartz.impl.jdbcjobstore.PostgreSQLDelegate
+    **/
+
+    @Override
+    protected Object getObjectFromBlob(ResultSet rs, String colName)
+            throws ClassNotFoundException, IOException, SQLException {
+        InputStream binaryInput = null;
+        byte[] bytes = rs.getBytes(colName);
+
+        Object obj = null;
+
+        if(bytes != null && bytes.length != 0) {
+            binaryInput = new ByteArrayInputStream(bytes);
+
+            ObjectInputStream in = new ObjectInputStream(binaryInput);
+            try {
+                obj = in.readObject();
+            } finally {
+                in.close();
+            }
+
+        }
+
+        return obj;
+    }
+
+    @Override
+    protected Object getJobDataFromBlob(ResultSet rs, String colName)
+            throws ClassNotFoundException, IOException, SQLException {
+        if (canUseProperties()) {
+            InputStream binaryInput = null;
+            byte[] bytes = rs.getBytes(colName);
+            if(bytes == null || bytes.length == 0) {
+                return null;
+            }
+            binaryInput = new ByteArrayInputStream(bytes);
+            return binaryInput;
+        }
+        return getObjectFromBlob(rs, colName);
+    }
+
+
+}
