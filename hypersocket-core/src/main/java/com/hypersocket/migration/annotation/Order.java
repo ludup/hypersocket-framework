5b07e8bb9291376b76d43a0b9758f2db74bd25be
gaurav@hypersocket.com <gaurav@hypersocket.com>
2017-03-27 19:55:24 +0530

    order annotation retention policy added

--- hypersocket-core/src/main/java/com/hypersocket/migration/annotation/Order.java
+++ hypersocket-core/src/main/java/com/hypersocket/migration/annotation/Order.java
@@ -1,5 +1,10 @@
 package com.hypersocket.migration.annotation;
 
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface Order {
     enum Direction {ASC, DESC}
 
