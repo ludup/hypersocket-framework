427b07da8ed97dafe88f292889e014dd60521d19
gaurav@hypersocket.com <gaurav@hypersocket.com>
2017-04-05 18:48:39 +0530

    Added legacy id and hook to execute custom logic during import and export

--- hypersocket-core/src/main/java/com/hypersocket/repository/AbstractEntity.java
+++ hypersocket-core/src/main/java/com/hypersocket/repository/AbstractEntity.java
@@ -120,6 +120,7 @@ public abstract class AbstractEntity<T> {
 		return this.getClass().getCanonicalName();
 	}
 
+	@JsonIgnore
 	public T getLegacyId() {
 		return legacyId == null ? getId() : legacyId;
 	}
