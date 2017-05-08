53ab9d5bf3e5960391f2000a8264a93ee858349f
ludup <lee@ludup.com>
2017-05-08 15:52:16 +0100

    Moved password template.

---
+++ hypersocket-core/src/main/java/com/hypersocket/auth/PasswordTemplate.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Hypersocket Limited.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ ******************************************************************************/
+package com.hypersocket.auth;
+
+import java.util.Map;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.hypersocket.i18n.I18N;
+import com.hypersocket.input.FormTemplate;
+import com.hypersocket.input.PasswordInputField;
+
+@XmlRootElement(name = "form")
+public class PasswordTemplate extends FormTemplate {
+
+	public static final String PASSWORD_FIELD = "password";
+
+	@SuppressWarnings("rawtypes")
+	public PasswordTemplate(
+			AuthenticationState state, Map params) {
+
+		fields.add(new PasswordInputField(PASSWORD_FIELD, "", true, I18N.getResource(state.getLocale(),
+				AuthenticationServiceImpl.RESOURCE_BUNDLE, "password.label")));
+
+	}
+
+}
