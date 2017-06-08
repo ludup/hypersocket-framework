36c549f89f78c48c84ee8e95af94a142dcadf5db
ludup <lee@ludup.com>
2017-06-08 10:51:17 +0100

    New Secure Node Agent extension target.

--- hypersocket-common/src/main/java/com/hypersocket/extensions/ExtensionTarget.java
+++ hypersocket-common/src/main/java/com/hypersocket/extensions/ExtensionTarget.java
@@ -1,5 +1,5 @@
 package com.hypersocket.extensions;
 
 public enum ExtensionTarget {
-	SERVER, CLIENT_SERVICE, CLIENT_GUI, VIRTUAL_MACHINE
+	SERVER, CLIENT_SERVICE, CLIENT_GUI, VIRTUAL_MACHINE, SECURE_NODE_AGENT
 }
