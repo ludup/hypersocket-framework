8038523e37b2500f99683627d7e4ccaa181d0188
ludup <lee@ludup.com>
2017-06-25 10:54:24 +0100

    Make generatePassword static.

--- hypersocket-tasks/src/main/java/com/hypersocket/tasks/user/password/GeneratePasswordTask.java
+++ hypersocket-tasks/src/main/java/com/hypersocket/tasks/user/password/GeneratePasswordTask.java
@@ -126,11 +126,11 @@ public class GeneratePasswordTask extends AbstractTaskProvider {
 		}
 	}
 	
-	public String generatePassword(int digits, 
+	public static String generatePassword(int digits, 
 			int lower, 
 			int upper, 
 			int symbols, 
-			int maxLength) throws AccessDeniedException, ResourceException {
+			int maxLength) {
 		
 		List<CharacterRule> rules = new ArrayList<CharacterRule>();
 		
