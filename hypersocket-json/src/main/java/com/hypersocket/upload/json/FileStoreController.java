d558fbef083ed2053e0489af5e5b2ea8ce7d95c7
ludup <lee@ludup.com>
2017-09-20 18:46:25 +0100

    Don't touch session on file download.

--- hypersocket-json/src/main/java/com/hypersocket/upload/json/FileStoreController.java
+++ hypersocket-json/src/main/java/com/hypersocket/upload/json/FileStoreController.java
@@ -24,6 +24,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.hypersocket.auth.json.AuthenticationRequired;
+import com.hypersocket.auth.json.AuthenticationRequiredButDontTouchSession;
 import com.hypersocket.auth.json.ResourceController;
 import com.hypersocket.auth.json.UnauthorizedException;
 import com.hypersocket.i18n.I18N;
@@ -225,7 +226,7 @@ public class FileStoreController extends ResourceController {
 		}
 	}
 
-	@AuthenticationRequired
+	@AuthenticationRequiredButDontTouchSession
 	@RequestMapping(value = "files/download/{uuid}", method = RequestMethod.GET)
 	public void downloadFile(HttpServletRequest request,
 			HttpServletResponse response, @PathVariable String uuid)
@@ -235,7 +236,7 @@ public class FileStoreController extends ResourceController {
 			resourceService.downloadURIFile(uuid, request, response, true, false);
 	}
 	
-	@AuthenticationRequired
+	@AuthenticationRequiredButDontTouchSession
 	@RequestMapping(value = "files/download/{uuid}/{filename}", method = RequestMethod.GET)
 	public void downloadFile(HttpServletRequest request,
 			HttpServletResponse response, @PathVariable String uuid, 
