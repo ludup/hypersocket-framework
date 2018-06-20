be151b026c420bb11f76699d3470c97420bc1d43
ludup <lee@ludup.com>
2018-06-20 13:26:32 +0100

    Delete user after revoking permissions.
    # Conflicts:
    #	hypersocket-core/src/main/java/com/hypersocket/realm/RealmServiceImpl.java

--- hypersocket-core/src/main/java/com/hypersocket/realm/RealmServiceImpl.java
+++ hypersocket-core/src/main/java/com/hypersocket/realm/RealmServiceImpl.java
@@ -1657,8 +1657,7 @@ public class RealmServiceImpl extends PasswordEnabledAuthenticatedServiceImpl
 
 			permissionService.revokePermissions(user, new TransactionAdapter<Principal>() {
 				@Override
-				public void beforeOperation(Principal resource, Map<String, String> properties)
-						throws ResourceException {
+				public void afterOperation(Principal resource, Map<String, String> properties) throws ResourceException {
 					try {
 						provider.deleteUser(resource);
 					} catch (ResourceChangeException e) {
