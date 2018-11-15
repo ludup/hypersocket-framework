b9427e8ffefc20e10e127740eecd864911dc2f68
ludup <lee@ludup.com>
2018-11-15 15:16:56 +0000

    Ability to remove object from export.
    # Conflicts:
    #	hypersocket-core/src/main/java/com/hypersocket/resource/AbstractAssignableResourceServiceImpl.java

--- hypersocket-core/src/main/java/com/hypersocket/resource/AbstractAssignableResourceServiceImpl.java
+++ hypersocket-core/src/main/java/com/hypersocket/resource/AbstractAssignableResourceServiceImpl.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.lang.reflect.Field;
 import java.math.BigInteger;
 import java.security.SecureRandom;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
@@ -608,7 +609,7 @@ public abstract class AbstractAssignableResourceServiceImpl<T extends Assignable
 		final T resource = getResourceById(id);
 		return exportResources(true, resource);
 	}
-
+	
 	@Override
 	public String exportAllResoures() throws ResourceExportException {
 		List<T> list = getResources();
@@ -624,14 +625,15 @@ public abstract class AbstractAssignableResourceServiceImpl<T extends Assignable
 		return false;
 	}
 	
-	protected void prepareExport(T resource) {
-		prepareExport(resource, true);
+	protected boolean prepareExport(T resource) {
+		return prepareExport(resource, true);
 	}
 	
-	protected void prepareExport(T resource, boolean stripIdentity) {
+	protected boolean prepareExport(T resource, boolean stripIdentity) {
 		if(isExportingAdditionalProperties()) {
 			resource.setProperties(getRepository().getProperties(resource));
 		}
+		return true;
 	}
 	
 	protected void prepareImport(T resource, Realm realm) throws ResourceException, AccessDeniedException {
@@ -651,18 +653,22 @@ public abstract class AbstractAssignableResourceServiceImpl<T extends Assignable
 		}
 		
 		ObjectMapper mapper = new ObjectMapper();
+		List<T> exported = new  ArrayList<T>();
+		
 		try {
 			for(T resource : resources) {
-				prepareExport(resource, stripIdentity);
-				if(stripIdentity) {
-					resource.setLegacyId(resource.getId());
-					resource.setId(null);
-					resource.setRealm(null);
-					resource.getRoles().clear();
+				if(prepareExport(resource, stripIdentity)) {
+					if(stripIdentity) {
+						resource.setLegacyId(resource.getId());
+						resource.setId(null);
+						resource.setRealm(null);
+						resource.getRoles().clear();
+					}
+					exported.add(resource);
 				}
 			}
 
-			return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(resources);
+			return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(exported);
 		} catch (JsonProcessingException e) {
 			throw new ResourceExportException(RESOURCE_BUNDLE_DEFAULT, "error.exportError", e.getMessage());
 		}
@@ -705,24 +711,27 @@ public abstract class AbstractAssignableResourceServiceImpl<T extends Assignable
 	
 	protected void performImport(T resource, Realm realm) throws ResourceException, AccessDeniedException {
 		resource.setRealm(realm);
-		checkImportName(resource, realm);
-		createResource(resource, resource.getProperties()==null ? new HashMap<String,String>() : resource.getProperties());
-		onResourceImported(resource);
+		if(checkImportName(resource, realm)) {
+			createResource(resource, resource.getProperties()==null ? new HashMap<String,String>() : resource.getProperties());
+			onResourceImported(resource);
+		}
+		
 	}
 	
 	protected void onResourceImported(T resource) {
 		
 	}
-	
-	protected void checkImportName(T resource, Realm realm) throws ResourceException, AccessDeniedException {
 
-		prepareImport(resource, isSystemResource() ? realmService.getSystemRealm() : realm);
+	protected boolean checkImportName(T resource, Realm realm) throws ResourceException, AccessDeniedException {
 		
-		String name = resource.getName();
-		int i=1;
-		while(!checkUnique(resource, true)) {
-			resource.setName(String.format("%s [#%d]", name, i++));
+		try {
+			prepareImport(resource, realm);
+			getResourceByName(resource.getName(), realm);
+			resource.setName(resource.getName() + " [imported]");
+		} catch(ResourceNotFoundException e) {
 		}
+		
+		return true;
 	}
 	
 
