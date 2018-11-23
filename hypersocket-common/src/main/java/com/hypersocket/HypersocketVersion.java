0424693d2ecb426666b42b8158c8821c387bdc8c
brett-smith <brett@nervepoint.com>
2018-11-23 12:55:03 +0000

    #LB12 - Hypersocket Client (now LogonBox Remote Access Client) will not connect from a Linux system

--- hypersocket-common/src/main/java/com/hypersocket/HypersocketVersion.java
+++ hypersocket-common/src/main/java/com/hypersocket/HypersocketVersion.java
@@ -55,7 +55,10 @@ public class HypersocketVersion {
 	    // try to load from maven properties first
 	    try {
 	        Properties p = new Properties();
-	        InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("/META-INF/maven/com.hypersocket/" + artifactId + "/pom.properties");
+	        InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("META-INF/maven/com.hypersocket/" + artifactId + "/pom.properties");
+	        if(is == null) {
+		        is = HypersocketVersion.class.getResourceAsStream("/META-INF/maven/com.hypersocket/" + artifactId + "/pom.properties");
+	        }
 	        if (is != null) {
 	            p.load(is);
 	            version = p.getProperty("version", "");
