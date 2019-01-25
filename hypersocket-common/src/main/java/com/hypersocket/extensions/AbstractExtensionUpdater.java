77b311f4a5cb45119b4ff546b8f2103b6ed65ff8
brett-smith <brett@nervepoint.com>
2019-01-25 11:28:12 +0000

    Stray import.

--- hypersocket-common/src/main/java/com/hypersocket/extensions/AbstractExtensionUpdater.java
+++ hypersocket-common/src/main/java/com/hypersocket/extensions/AbstractExtensionUpdater.java
@@ -12,7 +12,6 @@ import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
