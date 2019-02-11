6e97ae248cc1ac1780e902d2074e22f3c88fd2d5
ludup <lee@ludup.com>
2019-02-11 14:39:24 +0000

    Extended use of html and img input fields.

--- hypersocket-common/src/main/java/com/hypersocket/input/HtmlField.java
+++ hypersocket-common/src/main/java/com/hypersocket/input/HtmlField.java
@@ -2,11 +2,22 @@ package com.hypersocket.input;
 
 public class HtmlField extends InputField {
 
+	String classes;
+	
 	public HtmlField() {
 	}
 
 	public HtmlField(String resourceKey, String defaultValue) {
 		super(InputFieldType.html, resourceKey, defaultValue, true, null);
+	}
+	
+	public HtmlField(String resourceKey, String defaultValue, String classes) {
+		super(InputFieldType.html, resourceKey, defaultValue, true, null);
+		this.classes = classes;
+	}
+	
+	public String getClasses() {
+		return classes;
 	}
 
 }
--- hypersocket-common/src/main/java/com/hypersocket/input/ImgField.java
+++ hypersocket-common/src/main/java/com/hypersocket/input/ImgField.java
@@ -2,10 +2,21 @@ package com.hypersocket.input;
 
 public class ImgField extends InputField {
 
+	String url;
+	
 	public ImgField() {
 	}
 
 	public ImgField(String resourceKey, String defaultValue) {
 		super(InputFieldType.img, resourceKey, defaultValue, true, null);
+	}
+	
+	public ImgField(String resourceKey, String defaultValue, String url) {
+		super(InputFieldType.img, resourceKey, defaultValue, true, null);
+		this.url = url;
+	}
+	
+	public String getUrl() {
+		return url;
 	}
 }
