4494e8500cccd98e8ca04c62d0cb97f1dabe0af6
brett-smith <brett@nervepoint.com>
2019-02-26 14:48:04 +0000

    Getting of all users needs to be a single transaction in this case.

--- hypersocket-core/src/main/java/com/hypersocket/realm/RealmServiceImpl.java
+++ hypersocket-core/src/main/java/com/hypersocket/realm/RealmServiceImpl.java
@@ -34,6 +34,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionCallback;
 
 import com.hypersocket.attributes.AttributeType;
@@ -417,6 +418,7 @@ public class RealmServiceImpl extends PasswordEnabledAuthenticatedServiceImpl
 	}
 
 	@Override
+	@Transactional
 	public Set<Principal> getUsers(Realm realm, int max) {
 		return getAllPrincipals(realm, max, PrincipalType.USER);
 	}
