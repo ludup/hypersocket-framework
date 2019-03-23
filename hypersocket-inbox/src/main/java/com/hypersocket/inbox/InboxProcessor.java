4f4b332e401a6f04d6fa7993bf314ef9cdb207de
Lee Painter <lee@javassh.com>
2019-03-23 12:54:47 +0000

    Removed main.

--- hypersocket-inbox/src/main/java/com/hypersocket/inbox/InboxProcessor.java
+++ hypersocket-inbox/src/main/java/com/hypersocket/inbox/InboxProcessor.java
@@ -175,34 +175,4 @@ public class InboxProcessor {
 
 		return listAddress;
 	}
-
-	/**
-	 * Test downloading e-mail messages
-	 * @throws MessagingException 
-	 */
-	public static void main(String[] args) throws MessagingException {
-		// for POP3
-		// String protocol = "pop3";
-		// String host = "pop.gmail.com";
-		// String port = "995";
-
-		// for IMAP
-		String protocol = "imap";
-		String host = "imap.gmail.com";
-		String port = "993";
-
-		String userName = "lee@ninevehcottages.com";
-		String password = "xxxxxxx";
-
-		InboxProcessor receiver = new InboxProcessor();
-		receiver.downloadEmails(protocol, host, port, userName, password, true, true, new EmailProcessor() {
-
-			@Override
-			public void processEmail(Address[] from, Address[] replyTo, Address[] to, Address[] cc, String subject,
-					String textContent, String htmlContent, Date sent, Date received, EmailAttachment... attachments) {
-				// TODO Auto-generated method stub
-
-			}
-		});
-	}
 }
