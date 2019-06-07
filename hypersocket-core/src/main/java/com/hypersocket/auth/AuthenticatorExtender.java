1599f6e173b963d0c262a1922786b82a05f1a914
Lee Painter <lee@javassh.com>
2019-06-07 20:39:58 +0100

    Add canAuthenticate to AuthenticatorExtender.

--- hypersocket-core/src/main/java/com/hypersocket/auth/AuthenticatorExtender.java
+++ hypersocket-core/src/main/java/com/hypersocket/auth/AuthenticatorExtender.java
@@ -3,9 +3,12 @@ package com.hypersocket.auth;
 import java.util.Map;
 
 import com.hypersocket.permissions.AccessDeniedException;
+import com.hypersocket.realm.Principal;
 
 public interface AuthenticatorExtender {
 
 	public AuthenticatorResult authenticate(AuthenticationState state, @SuppressWarnings("rawtypes") Map parameters) throws AccessDeniedException;
+
+	boolean canAuthenticate(Principal principal);
 		
 }
