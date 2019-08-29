4ec7e792dd08153cf913f13b3ad6f72a98535d94
Brett Smith <brett@logonbox.com>
2019-08-29 14:56:41 +0100

    Compile error from type and catch exception. @ludup please review, I am not sure of intentions here if this should be a hard error or just-log-and-continue.

--- hypersocket-core/src/main/java/com/hypersocket/encrypt/DefaultEncryptor.java
+++ hypersocket-core/src/main/java/com/hypersocket/encrypt/DefaultEncryptor.java
@@ -14,7 +14,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.hypersocket.permissions.AccessDeniedException;
 import com.hypersocket.realm.Realm;
+import com.hypersocket.resource.ResourceException;
 import com.hypersocket.secret.SecretKeyResource;
 import com.hypersocket.secret.SecretKeyService;
 
@@ -57,8 +59,13 @@ public class DefaultEncryptor implements Encryptor {
 			return Base64.encodeBase64String(byteCipherText);
 		} catch (Exception e) {
 			if(existingKey) {
-				secretKeyService.deleteSecretkey(reference, realm);
-				return encryptString(reference, data, realm);
+				try {
+					secretKeyService.deleteSecretKey(reference, realm);
+					return encryptString(reference, data, realm);
+				} catch (ResourceException | AccessDeniedException e1) {
+					/* Just log this so the actual error that caused this is not lost **/
+					log.error("Failed to remove existing secret key while attempting to clear up after an exception.", e1);
+				}
 			}
 			throw new IOException(e.getMessage(), e);
 		}
