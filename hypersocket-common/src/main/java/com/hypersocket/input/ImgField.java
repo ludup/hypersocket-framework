60825ac7195fc94249f4fd12bc053d8ae13d4630
Lee Painter <lee@javassh.com>
2019-09-03 17:07:48 +0100

    Ability to set alt, width and class on img fields.

--- hypersocket-common/src/main/java/com/hypersocket/input/ImgField.java
+++ hypersocket-common/src/main/java/com/hypersocket/input/ImgField.java
@@ -3,6 +3,10 @@ package com.hypersocket.input;
 public class ImgField extends InputField {
 
 	String url;
+	String alt;
+	Integer width;
+	String _float;
+	String _class;
 	
 	public ImgField() {
 	}
@@ -12,11 +16,38 @@ public class ImgField extends InputField {
 	}
 	
 	public ImgField(String resourceKey, String defaultValue, String url) {
+		this(resourceKey, defaultValue, url, null, null, null);
+	}
+	
+	public ImgField(String resourceKey, String defaultValue, String url, String alt, Integer width) {
+		this(resourceKey, defaultValue, url, alt, width, null);
+	}
+	
+	public ImgField(String resourceKey, String defaultValue, String url, String alt, Integer width, String _class) {
 		super(InputFieldType.img, resourceKey, defaultValue, true, null);
 		this.url = url;
+		this.alt = alt;
+		this.width = width;
+		this._class = _class;
 	}
 	
 	public String getUrl() {
 		return url;
+	}
+
+	public Integer getWidth() {
+		return width;
+	}
+
+	public void setWidth(Integer width) {
+		this.width = width;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	public String getStyleClass() {
+		return _class;
 	}
 }
