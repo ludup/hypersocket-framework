319aeabf7b93af634ce59507db664d4cde105fd8
brett-smith <brett@nervepoint.com>
2020-08-03 16:07:09 +0100

    Should have been a pre.sql

--- hypersocket-core/src/main/resources/upgrade/core_2_DOT_2_DOT_2.sql
+++
@@ -1,4 +0,0 @@
-EXIT IF FRESH;
-ALTER TABLE session RENAME COLUMN 'system' TO 'system_session';
-ALTER TABLE resources RENAME COLUMN 'system' TO 'system_resource';
-ALTER TABLE permissions RENAME COLUMN 'system' TO 'system_permission';
