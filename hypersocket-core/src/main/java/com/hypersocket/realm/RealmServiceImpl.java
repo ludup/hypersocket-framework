7efde410ccedf42a0de9f472bcda1ebf3a2fc2fa
Lee Painter <lee@ludup.com>
2020-12-05 13:57:08 +0000

    New realms use visible properties only. CORS add request methods header.

--- hypersocket-core/src/main/java/com/hypersocket/realm/RealmServiceImpl.java
+++ hypersocket-core/src/main/java/com/hypersocket/realm/RealmServiceImpl.java
@@ -1214,7 +1214,8 @@ public class RealmServiceImpl extends PasswordEnabledAuthenticatedServiceImpl
 								oldProvider.resetRealm(realm);
 								realmProvider.resetRealm(realm);
 
-								configurationService.setValue(realm, "realm.userEditableProperties",
+								configurationService.setValue(realm, "realm.userEditableProperties", "");
+								configurationService.setValue(realm, "realm.userVisibleProperties",
 										ResourceUtils.implodeValues(realmProvider.getDefaultUserPropertyNames()));
 
 								realm.setReadOnly(false);
