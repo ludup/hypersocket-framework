e76f7e2ee6bb61fb8cdddda6cb33efccd337305f
Brett Smith <brett@logonbox.com>
2021-01-13 01:37:59 +0000

    Build error.

--- hypersocket-core/src/main/java/com/hypersocket/geo/IStackService.java
+++ hypersocket-core/src/main/java/com/hypersocket/geo/IStackService.java
@@ -2,6 +2,8 @@ package com.hypersocket.geo;
 
 import java.io.IOException;
 
+import com.hypersocket.session.IStackLocation;
+
 public interface IStackService {
 
 	IStackLocation lookupGeoIP(String ipAddress) throws IOException;
