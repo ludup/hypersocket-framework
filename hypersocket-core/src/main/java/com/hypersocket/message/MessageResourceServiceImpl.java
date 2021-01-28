24af8d1805004ffca1e3e3b7eaa85e340766bacf
Brett Smith <brett@logonbox.com>
2021-01-28 15:55:05 +0000

    Make sure PrincipalWithoutPasswordResolver and any resolver that extends it is constructed inside a transaction. This is because group details are now required, and they are marked for lazy initialisation.

--- hypersocket-core/src/main/java/com/hypersocket/message/MessageResourceServiceImpl.java
+++ hypersocket-core/src/main/java/com/hypersocket/message/MessageResourceServiceImpl.java
@@ -30,6 +30,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.hypersocket.email.EmailAttachment;
 import com.hypersocket.email.EmailBatchService;
@@ -401,6 +402,7 @@ public class MessageResourceServiceImpl extends AbstractResourceServiceImpl<Mess
 	}
 
 	@Override
+	@Transactional
 	public void test(MessageResource message, String email) {
 		UserPrincipal<?> user = (UserPrincipal<?>) getCurrentPrincipal();
 		Collection<String> recipients = StringUtils.isEmpty(email) ? null : Arrays.asList(email);
