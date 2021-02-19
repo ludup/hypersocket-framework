97869d6dd8b63e4781987fd080170c419043a298
Lee Painter <lee@ludup.com>
2021-02-19 12:56:28 +0000

    Bringing logonbox brand into core.

--- hypersocket-core/src/main/java/com/hypersocket/message/FreeMarkerServiceImpl.java
+++ hypersocket-core/src/main/java/com/hypersocket/message/FreeMarkerServiceImpl.java
@@ -17,7 +17,6 @@ public class FreeMarkerServiceImpl implements FreeMarkerService {
 	private Configuration cfg;
 	private StringTemplateLoader stringLoader = new StringTemplateLoader();
 	
-	@SuppressWarnings("deprecation")
 	@PostConstruct
 	private void postConstruct() {
 		
