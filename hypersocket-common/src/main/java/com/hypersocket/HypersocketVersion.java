b13b2626062e5ce5b36b55e3cbc2bd3fb9cf6c15
brett-smith <brett@nervepoint.com>
2021-07-17 10:21:13 +0100

    New preferred method to get version of extension artifact. Load the MANIFEST.MF from all jars looking for the X-Extension-Version attribute. Any jar that has the attribute also can optionally have an X-Extension-Priority attribute. The highest priority is the version that will be used.

--- hypersocket-common/src/main/java/com/hypersocket/HypersocketVersion.java
+++ hypersocket-common/src/main/java/com/hypersocket/HypersocketVersion.java
@@ -9,13 +9,17 @@ package com.hypersocket;
 
 import java.io.File;
 import java.io.InputStream;
+import java.net.URL;
+import java.util.Enumeration;
 import java.util.Properties;
 import java.util.UUID;
+import java.util.jar.Manifest;
 import java.util.prefs.Preferences;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
+import org.apache.commons.lang3.StringUtils;
 import org.w3c.dom.Document;
 
 public class HypersocketVersion {
@@ -51,11 +55,42 @@ public class HypersocketVersion {
 	    if (version != null) {
 	        return version;
 	    }
+	    
+	    /* Load the MANIFEST.MF from all jars looking for the X-Extension-Version
+	     * attribute. Any jar that has the attribute also can optionally have
+	     * an X-Extension-Priority attribute. The highest priority is the
+	     * version that will be used.
+	     */
+	    ClassLoader cl = Thread.currentThread().getContextClassLoader();
+	    if(cl == null)
+	    	cl = HypersocketVersion.class.getClassLoader();
+		try {
+			int highestPriority = -1;
+			String highestPriorityVersion = null;
+		    for(Enumeration<URL> en = cl.getResources("META-INF/MANIFEST.MF");
+		    		en.hasMoreElements(); ) {
+		    	URL url = en.nextElement();
+		    	try(InputStream in = url.openStream()) {
+			    	Manifest mf = new Manifest(in);	
+			    	String extensionVersion = mf.getMainAttributes().getValue("X-Extension-Version");
+			    	String priorityStr = mf.getMainAttributes().getValue("X-Extension-Priority");
+			    	int priority = StringUtils.isBlank(priorityStr) ? 0 : Integer.parseInt(priorityStr);
+			    	if(priority > highestPriority) {
+			    		highestPriorityVersion = extensionVersion;
+			    	}
+		    	}
+		    }
+		    if(highestPriorityVersion != null)
+		    	version = highestPriorityVersion;
+	    }
+	    catch(Exception e) {
+	    	
+	    }
 
 	    // try to load from maven properties first
 	    try {
 	        Properties p = new Properties();
-	        InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("META-INF/maven/com.hypersocket/" + artifactId + "/pom.properties");
+	        InputStream is = cl.getResourceAsStream("META-INF/maven/com.hypersocket/" + artifactId + "/pom.properties");
 	        if(is == null) {
 		        is = HypersocketVersion.class.getResourceAsStream("/META-INF/maven/com.hypersocket/" + artifactId + "/pom.properties");
 	        }
