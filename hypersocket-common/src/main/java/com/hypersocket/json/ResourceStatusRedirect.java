342a39e2c1448b7440ac1d33b87ff70d05fa7e54
brett-smith <brett@nervepoint.com>
2021-07-19 20:04:35 +0100

    Hrm. Why, this is in json-client too. really needs fixing once and for all.

---
+++ hypersocket-common/src/main/java/com/hypersocket/json/ResourceStatusRedirect.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2003-2020 JADAPTIVE Limited. All Rights Reserved.
+ *
+ * For product documentation visit https://www.jadaptive.com/
+ *
+ * This file is part of Hypersocket JSON Client.
+ *
+ * Hypersocket JSON Client is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Hypersocket JSON Client is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Hypersocket JSON Client.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.hypersocket.json;
+
+public class ResourceStatusRedirect<T> extends ResourceStatus<T> {
+
+	private boolean redirect = false;
+
+	public ResourceStatusRedirect(String message) {
+		setSuccess(false);
+		setMessage(message);
+		setRedirect(true);
+	}
+	
+	public boolean isRedirect() {
+		return redirect;
+	}
+
+	public void setRedirect(boolean redirect) {
+		this.redirect = redirect;
+	}
+}
