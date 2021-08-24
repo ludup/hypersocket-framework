4c2df84731d32c440b62e5db0e18d97fbdbf8ab4
Lee Painter <lee@javassh.com>
2021-08-24 11:31:44 +0100

    Capture change password errors and show accordingly allow user to try again.

--- hypersocket-core/src/main/java/com/hypersocket/auth/ChangePasswordTemplate.java
+++ hypersocket-core/src/main/java/com/hypersocket/auth/ChangePasswordTemplate.java
@@ -7,6 +7,8 @@
  ******************************************************************************/
 package com.hypersocket.auth;
 
+import org.apache.commons.lang3.StringUtils;
+
 import com.hypersocket.i18n.I18N;
 import com.hypersocket.input.FormTemplate;
 import com.hypersocket.input.ParagraphField;
@@ -17,10 +19,13 @@ public class ChangePasswordTemplate extends FormTemplate {
 	public static final String PASSWORD_FIELD = "password";
 	public static final String CONFIRM_PASSWORD_FIELD = "confirmPassword";
 	
-	public ChangePasswordTemplate(AuthenticationState state, String buttonResourceKey) {
+	public ChangePasswordTemplate(AuthenticationState state, String buttonResourceKey, String error, boolean errorIsResourceKey) {
 		super(state.getInitialSchemeResourceKey());
 		setResourceKey(UsernameAndPasswordAuthenticator.RESOURCE_KEY);
 
+		if(StringUtils.isNotBlank(error)) {
+			fields.add(new ParagraphField(error, errorIsResourceKey, true, "danger"));
+		}	
 		fields.add(new ParagraphField("info.passwordChangeRequired", true));
 		fields.add(new PasswordInputField(PASSWORD_FIELD, "", true, I18N
 				.getResource(state.getLocale(),
