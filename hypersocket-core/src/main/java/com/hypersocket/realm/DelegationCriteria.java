1f9c37cfef3a6bf9d8360b0c7821ff1019918dfb
Lee Painter <lee@javassh.com>
2021-09-20 21:32:17 +0100

    Added caching and checks to user delegation searches.

--- hypersocket-core/src/main/java/com/hypersocket/realm/DelegationCriteria.java
+++ hypersocket-core/src/main/java/com/hypersocket/realm/DelegationCriteria.java
@@ -5,17 +5,26 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.annotation.PostConstruct;
+import javax.cache.Cache;
+
 import org.hibernate.Criteria;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import com.hypersocket.cache.CacheService;
+import com.hypersocket.change.ResourceAssignmentChangeListener;
+import com.hypersocket.change.ResourceAssignmentChangeService;
 import com.hypersocket.delegation.UserDelegationResource;
 import com.hypersocket.delegation.UserDelegationResourceService;
+import com.hypersocket.delegation.events.UserDelegationResourceEvent;
 import com.hypersocket.permissions.PermissionService;
 import com.hypersocket.permissions.Role;
 import com.hypersocket.repository.CriteriaConfiguration;
 import com.hypersocket.repository.HibernateUtils;
+import com.hypersocket.role.events.RoleEvent;
 
 @Component
 public class DelegationCriteria implements CriteriaConfiguration {
@@ -29,48 +38,106 @@ public class DelegationCriteria implements CriteriaConfiguration {
 	@Autowired
 	private PermissionService permissionService;
 	
+	@Autowired
+	private CacheService cacheService;
+	
 	@Override
 	public void configure(Criteria criteria) {
 		
+		Principal currentUser = realmService.getCurrentPrincipal();
+		@SuppressWarnings("rawtypes")
+		Cache<Long,Collection> cachedRoleIds = cacheService.getCacheOrCreate("delegationQueryRoleIds", Long.class, Collection.class);
+		@SuppressWarnings("rawtypes")
+		Cache<Long,Collection> cachedUserIds = cacheService.getCacheOrCreate("delegationQueryUserIds", Long.class, Collection.class);
 		
-		Collection<UserDelegationResource> resources = delegationService.getPersonalResources();
-		Set<Role> delegatedRoles = new HashSet<>();
-		Set<Principal> delegatedPrincipals = new HashSet<>();
-		Set<Role> allUserRoles = permissionService.getAllUserRoles();
-		boolean everyone = false;
-		for(UserDelegationResource resource : resources) {
-			if(!Collections.disjoint(resource.getRoleDelegates(), allUserRoles)) {
-				everyone = true;
-				break;
-			}
-			delegatedRoles.addAll(resource.getRoleDelegates());
-			for(Principal principal : resource.getUserDelegates()) {
-				delegatedPrincipals.add(principal);
-			}
-			for(Principal principal : resource.getGroupDelegates()) {
+		if(cachedRoleIds.containsKey(realmService.getCurrentPrincipal().getId())) {
+			
+			int maximumRoles = Integer.parseInt(System.getProperty("hypersocket.maximumRoleDelegates", "100"));
+			int maximumUsers = Integer.parseInt(System.getProperty("hypersocket.maximumUserDelegates", "1000"));
+			
+			Collection<UserDelegationResource> resources = delegationService.getPersonalResources();
+			Set<Role> delegatedRoles = new HashSet<>();
+			Set<Principal> delegatedPrincipals = new HashSet<>();
+			Set<Role> allUserRoles = permissionService.getAllUserRoles();
+			boolean everyone = false;
+			for(UserDelegationResource resource : resources) {
+				if(!Collections.disjoint(resource.getRoleDelegates(), allUserRoles)) {
+					everyone = true;
+					break;
+				}
+				delegatedRoles.addAll(resource.getRoleDelegates());
 				
-				for(Principal assosciated : realmService.getAssociatedPrincipals(principal)) {
-					if(assosciated.getType()==PrincipalType.USER) {
-						delegatedPrincipals.add(assosciated);
-					} else if(assosciated.getType() == PrincipalType.GROUP) {
-						delegatedPrincipals.addAll(realmService.getAssociatedPrincipals(assosciated, PrincipalType.USER));
+				if(delegatedRoles.size() > maximumRoles) {
+					throw new IllegalStateException("Too many role delegates for principal query");
+				}
+				
+				delegatedPrincipals.addAll(resource.getUserDelegates());
+				
+				if(delegatedPrincipals.size() > maximumUsers) {
+					throw new IllegalStateException("Too many user delegates for principal query");
+				}
+				
+				for(Principal principal : resource.getGroupDelegates()) {
+					
+					for(Principal assosciated : realmService.getAssociatedPrincipals(principal)) {
+						if(assosciated.getType()==PrincipalType.USER) {
+							delegatedPrincipals.add(assosciated);
+						} else if(assosciated.getType() == PrincipalType.GROUP) {
+							delegatedPrincipals.addAll(realmService.getAssociatedPrincipals(assosciated, PrincipalType.USER));
+						}
+					}
+					
+					if(delegatedPrincipals.size() > maximumUsers) {
+						throw new IllegalStateException("Too many user delegates for principal query");
 					}
 				}
 			}
+			
+			if(everyone) {
+				return;
+			}
+			
+			cachedRoleIds.put(realmService.getCurrentPrincipal().getId(), HibernateUtils.getResourceIds(delegatedRoles));
+			cachedUserIds.put(realmService.getCurrentPrincipal().getId(), HibernateUtils.getResourceIds(delegatedPrincipals));
+		
 		}
 		
-		if(everyone) {
-			return;
-		}
+		@SuppressWarnings("unchecked")
+		Collection<Long> roleIds =  (Collection<Long>) cachedRoleIds.get(currentUser.getId());
+		@SuppressWarnings("unchecked")
+		Collection<Long> userIds =  (Collection<Long>) cachedUserIds.get(currentUser.getId());
 		
-		if(!delegatedRoles.isEmpty()) {
+		if(!roleIds.isEmpty()) {
 			criteria.createAlias("roles", "delegated");
-			criteria.add(Restrictions.in("delegated.id", HibernateUtils.getResourceIds(delegatedRoles)));
+			criteria.add(Restrictions.in("delegated.id", roleIds));
 		}
 		
-		if(!delegatedPrincipals.isEmpty()) {
-			criteria.add(Restrictions.in("id", HibernateUtils.getResourceIds(delegatedPrincipals)));
+		if(!userIds.isEmpty()) {
+			criteria.add(Restrictions.in("id", userIds));
 		}
 	}
 
+	@EventListener
+	public void onRoleChange(RoleEvent evt) {
+		if(evt.isSuccess()) {
+			@SuppressWarnings("rawtypes")
+			Cache<Long,Collection> cachedRoleIds = cacheService.getCacheOrCreate("delegationQueryRoleIds", Long.class, Collection.class);
+			cachedRoleIds.removeAll();
+			@SuppressWarnings("rawtypes")
+			Cache<Long,Collection> cachedUserIds = cacheService.getCacheOrCreate("delegationQueryUserIds", Long.class, Collection.class);
+			cachedUserIds.removeAll();
+		}
+	}
+	
+	@EventListener
+	public void onDelegationChange(UserDelegationResourceEvent evt) {
+		if(evt.isSuccess()) {
+			@SuppressWarnings("rawtypes")
+			Cache<Long,Collection> cachedRoleIds = cacheService.getCacheOrCreate("delegationQueryRoleIds", Long.class, Collection.class);
+			cachedRoleIds.removeAll();
+			@SuppressWarnings("rawtypes")
+			Cache<Long,Collection> cachedUserIds = cacheService.getCacheOrCreate("delegationQueryUserIds", Long.class, Collection.class);
+			cachedUserIds.removeAll();
+		}
+	}
 }
