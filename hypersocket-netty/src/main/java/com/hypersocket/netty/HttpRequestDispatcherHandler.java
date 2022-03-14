b434d215340f438610e2b49a9263e236eb542b31
brett-smith <brett@nervepoint.com>
2022-03-14 13:03:47 +0000

    Allow threads to do. Use scaling executor for workers, but ordered executor for execution handlers.

--- hypersocket-netty/src/main/java/com/hypersocket/netty/HttpRequestDispatcherHandler.java
+++ hypersocket-netty/src/main/java/com/hypersocket/netty/HttpRequestDispatcherHandler.java
@@ -13,7 +13,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
 import java.net.URL;
-import java.net.URLClassLoader;
 import java.net.URLConnection;
 import java.nio.channels.ClosedChannelException;
 import java.nio.charset.Charset;
