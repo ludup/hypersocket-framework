9a44a9888f580365985d5229581b678825c2dd09
Brett Smith <brett@logonbox.com>
2022-03-17 15:23:54 +0000

    Added RSA 4096 bit constant

--- hypersocket-core/src/main/java/com/hypersocket/certificates/CertificateType.java
+++ hypersocket-core/src/main/java/com/hypersocket/certificates/CertificateType.java
@@ -2,7 +2,7 @@ package com.hypersocket.certificates;
 
 
 public enum CertificateType {
-
-	RSA_1024, RSA_2048, DSA_1024, ECDSA_256, ECDSA_384, ECDSA_521;
+	/* NOTE: Do not change order */
+	RSA_1024, RSA_2048, DSA_1024, ECDSA_256, ECDSA_384, ECDSA_521, RSA_4096;
 
 }
