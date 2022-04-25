b6b58163db113559da33e085e6ec642a64d29db0
brett-smith <brett@nervepoint.com>
2022-04-25 10:16:47 +0100

    Sessions are never removed!

--- hypersocket-server/src/main/java/com/hypersocket/servlet/HypersocketSession.java
+++ hypersocket-server/src/main/java/com/hypersocket/servlet/HypersocketSession.java
@@ -23,7 +23,7 @@ public class HypersocketSession implements HttpSession {
 	private long creationTime = System.currentTimeMillis();
 	private UUID uuid = UUID.randomUUID();
 	private long lastAccessedTime = creationTime;
-	private Map<String,Object> attributes = new HashMap<String,Object>();
+	private Map<String,Object> attributes = new HashMap<>();
 	private boolean isNew = true;
 	private int maxInterval = 300;
 	private ServletContext context;
@@ -31,73 +31,89 @@ public class HypersocketSession implements HttpSession {
 	HypersocketSession( ServletContext context) {
 		this.context = context;
 	}
-	
+
 	void access() {
 		lastAccessedTime = System.currentTimeMillis();
 		isNew = false;
 	}
+	@Override
 	public long getCreationTime() {
 		return creationTime;
 	}
 
+	@Override
 	public String getId() {
 		return uuid.toString();
 	}
 
+	@Override
 	public long getLastAccessedTime() {
 		return lastAccessedTime;
 	}
 
+	@Override
 	public ServletContext getServletContext() {
 		return context;
 	}
 
+	@Override
 	public void setMaxInactiveInterval(int interval) {
 		this.maxInterval = interval;
 
 	}
 
+	@Override
 	public int getMaxInactiveInterval() {
 		return maxInterval;
 	}
 
+	@Override
 	public HttpSessionContext getSessionContext() {
 		throw new UnsupportedOperationException();
 	}
 
+	@Override
 	public Object getAttribute(String name) {
 		return attributes.get(name);
 	}
 
+	@Override
 	public Object getValue(String name) {
 		return getAttribute(name);
 	}
 
+	@Override
 	public Enumeration<String> getAttributeNames() {
-		return new Vector<String>(attributes.keySet()).elements();
+		return new Vector<>(attributes.keySet()).elements();
 	}
 
+	@Override
 	public String[] getValueNames() {
 		return attributes.keySet().toArray(new String[0]);
 	}
 
+	@Override
 	public void setAttribute(String name, Object value) {
 		attributes.put(name, value);
 
 	}
 
+	@Override
 	public void putValue(String name, Object value) {
 		setAttribute(name, value);
 	}
 
+	@Override
 	public void removeAttribute(String name) {
 		attributes.remove(name);
 	}
 
+	@Override
 	public void removeValue(String name) {
 		removeAttribute(name);
 	}
 
+	@Override
 	public void invalidate() {
 
 	}
