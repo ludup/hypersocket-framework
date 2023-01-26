cbd0b1f9d1755d2a35ca19c3ebcae29fe5320aa2
garrow <garrow>
2023-01-26 08:25:27 +0000

    #M454 - Many Authentication Modules colours are incorrect according to the legend
    Added new module category enum
    Modules reflect their category using new category enum

---
+++ hypersocket-core/src/main/java/com/hypersocket/auth/AuthenticationModuleCategory.java
@@ -0,0 +1,15 @@
+package com.hypersocket.auth;
+
+public enum AuthenticationModuleCategory {
+	SECRET, IDENTITY, IDENTITY_SECRET, AUTHENTICATION_FLOW, OTHER 
+}
+
+
+
+/**
+Blue: IDENTITY
+Green: SECRET
+Orange: IDENTITY_SECRET
+Grey: OTHER
+Purple: AUTHENTICATION_FLOW
+**/
