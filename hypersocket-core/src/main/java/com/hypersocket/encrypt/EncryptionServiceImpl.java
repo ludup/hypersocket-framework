6b283538e4cfe91951e6eedddc047b4f6d23288e
brett-smith <brett@nervepoint.com>
2023-06-15 16:15:58 +0100

    Lazy initialize

--- hypersocket-core/src/main/java/com/hypersocket/encrypt/EncryptionServiceImpl.java
+++ hypersocket-core/src/main/java/com/hypersocket/encrypt/EncryptionServiceImpl.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.event.ContextStartedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
@@ -28,6 +29,7 @@ public class EncryptionServiceImpl implements EncryptionService {
 	
 	@Autowired
 	@Qualifier("defaultEncryptor")
+	@Lazy
 	private Encryptor encryptor;
 
 	@Autowired
