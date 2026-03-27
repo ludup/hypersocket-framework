4053e31e99fb668d936f30c3502670143ae995ee
garrow <garrow>
2026-03-27 06:03:20 +0000

    #M880 - Block Hazelcast update checks
    Added boolean property to stop call home.

--- hypersocket-core/src/main/java/com/hypersocket/spring/jconfig/HazelcastSpringConfiguration.java
+++ hypersocket-core/src/main/java/com/hypersocket/spring/jconfig/HazelcastSpringConfiguration.java
@@ -63,6 +63,9 @@ public class HazelcastSpringConfiguration {
 		config.setInstanceName(applicationContext.getId());
 		config.setNetworkConfig(networkConfig);
 		config.setProperty("hazelcast.logging.type", "log4j2");
+		
+		config.setProperty("hazelcast.version.check.enabled", "false");
+
 		if(Boolean.getBoolean("hypersocket.development")) {
 			config.setProperty("hazelcast.operation.thread.count", "4");
 			config.setProperty("hazelcast.event.thread.count", "3");
